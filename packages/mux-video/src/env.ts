bfceb31d67bf6a7e2bf1a44b59ad089001fb8b01
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2021-11-16 08:12:40 -0800

    build/es2019 -- build for 0.1.6-beta.2

--- packages/mux-video/src/env.ts
+++ packages/mux-video/src/env.ts
@@ -3,20 +3,10 @@ export const isMaybeBrowser = () => typeof window != "undefined";
 export const isMaybeServer = () => typeof global != "undefined";
 
 const getEnvPlayerVersion = () => {
-  if (isMaybeBrowser()) {
-    try {
-      // @ts-ignore
-      import.meta.env;
-      // @ts-ignore
-      return __SNOWPACK_ENV__?.PLAYER_VERSION as string;
-    } catch {}
-  }
-  if (isMaybeServer()) {
-    try {
-      // @ts-ignore
-      return PLAYER_VERSION as string;
-    } catch {}
-  }
+  try {
+    // @ts-ignore
+    return PLAYER_VERSION as string;
+  } catch {}
   return "UNKNOWN";
 };
 
