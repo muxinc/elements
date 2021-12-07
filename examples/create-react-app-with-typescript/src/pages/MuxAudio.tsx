8f76f6cd00fa22ec3ef1bd323d67d1f355308002
Christian Pillsbury <cjpillsbury@gmail.com>
2021-12-07 15:21:51 -0800

    improv/media-code-sharing: Add mux-audio-react to examples.

---
+++ examples/create-react-app-with-typescript/src/pages/MuxAudio.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import MuxAudio from "@mux-elements/mux-audio-react";
+
+function MuxVideoPage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexFlow: "column",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <h1>MuxAudio Demo</h1>
+      <div style={{ flexGrow: 1, flexShrink: 1 }}>
+        <MuxAudio
+          style={{ maxWidth: "100%" }}
+          playbackId="DS00Spx1CV902MCtPj5WknGlR102V5HFkDe"
+          // metadata={{
+          //   video_id: "video-id-12345",
+          //   video_title: "Mad Max: Fury Road Trailer",
+          //   viewer_user_id: "user-id-6789",
+          // }}
+          // envKey="mux-data-env-key"
+          streamType="on-demand"
+          controls
+          autoPlay
+          muted
+        />
+      </div>
+      <h3 className="title">
+        <Link to="/">Browse Elements</Link>
+      </h3>
+    </div>
+  );
+}
+
+export default MuxVideoPage;
