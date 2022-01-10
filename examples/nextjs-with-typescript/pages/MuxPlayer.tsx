008539426cd59ac14979e63526c2108451773e65
Christian Pillsbury <cjpillsbury@gmail.com>
2022-01-10 09:10:02 -0600

    feat/mux-video-react: add MuxPlayer page to next example app.

---
+++ examples/nextjs-with-typescript/pages/MuxPlayer.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import MuxPlayer from "@mux-elements/mux-player-react";
+
+function MuxVideoPage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexFlow: "column",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <h1>MuxVideo Demo</h1>
+      <div style={{ flexGrow: 1, flexShrink: 1, height: "400px" }}>
+        <MuxPlayer
+          playbackId="ddBx5002F02xe7ftFvTFkYBxEdQ2inQ2o029CMqu9A4IcY"
+          debug
+          muted
+          startTime={12}
+        />
+      </div>
+      <h3 className="title">
+        <Link href="/">
+          <a>Browse Elements</a>
+        </Link>
+      </h3>
+    </div>
+  );
+}
+
+export default MuxVideoPage;
