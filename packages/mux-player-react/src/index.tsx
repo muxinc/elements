b32bef5c80b80f13f12e2ed6f5deff093ee41027
Christian Pillsbury <cjpillsbury@gmail.com>
2022-01-10 20:26:33 -0600

    feat/mux-player-react: Add responsive chrome stuff. Various cleanup.

--- packages/mux-player-react/src/index.tsx
+++ packages/mux-player-react/src/index.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
+import * as CSS from "csstype";
 import MuxVideo from "@mux-elements/mux-video-react";
 import type { Props as MuxVideoProps } from "@mux-elements/mux-video-react";
 import {
@@ -22,6 +23,11 @@ import { StreamTypes } from "@mux-elements/playback-core";
 
 export { StreamTypes };
 
+const MediaChromeSizes = {
+  LG: "large",
+  SM: "small",
+};
+
 declare global {
   interface Window {
     WebKitPlaybackTargetAvailabilityEvent?: Event;
@@ -32,6 +38,47 @@ declare global {
   }
 }
 
+declare module "csstype" {
+  interface Properties {
+    "--media-background-color"?: CSS.Properties["backgroundColor"];
+    "--media-control-background"?: CSS.Properties["backgroundColor"];
+    "--media-button-icon-width"?: CSS.Properties["width"];
+    // ...or allow any other property
+    // [index: string]: any;
+  }
+}
+
+const useEffectInEvent = (
+  event: "resize" | "scroll",
+  set: () => void = () => {},
+  useCapture: boolean = false
+) => {
+  useEffect(() => {
+    set();
+    window.addEventListener(event, set, useCapture);
+    return () => window.removeEventListener(event, set, useCapture);
+  }, []);
+};
+
+export const useRect = <T extends Element>(): [
+  DOMRect | undefined,
+  React.MutableRefObject<T | null> | ((node?: T) => void)
+] => {
+  const ref = useRef<T>();
+  const [rect, setRect] = useState<DOMRect>();
+
+  const set = () => setRect(ref.current?.getBoundingClientRect());
+
+  useEffectInEvent("resize", set);
+  useEffectInEvent("scroll", set, true);
+  const refCb = useCallback((node?: T) => {
+    ref.current = node;
+    set();
+  }, []);
+
+  return [rect, refCb];
+};
+
 const getPosterURLFromPlaybackId = (playbackId: MuxVideoProps["playbackId"]) =>
   `https://image.mux.com/${playbackId}/thumbnail.jpg`;
 
@@ -39,13 +86,66 @@ const getStoryboardURLFromPlaybackId = (
   playbackId: MuxVideoProps["playbackId"]
 ) => `https://image.mux.com/${playbackId}/storyboard.vtt`;
 
+const Spacer = () => {
+  return (
+    <div
+      style={{
+        flexGrow: 1,
+        height: "100%",
+        backgroundColor: "var(--media-control-background, rgba(20,20,30, 0.7))",
+      }}
+    ></div>
+  );
+};
+
 type ChromeProps = {
   onAirPlaySelected?: React.MouseEventHandler;
   hasAirPlay?: boolean;
   streamType?: MuxVideoProps["streamType"];
+  playerSize?: string;
 };
 
-export const VodChromeSmall = () => {};
+export const VodChromeSmall: React.FC<ChromeProps> = (props) => {
+  const { onAirPlaySelected, hasAirPlay = false } = props;
+  return (
+    <>
+      <div
+        slot="centered-chrome"
+        style={{
+          "--media-background-color": "none",
+          "--media-control-background": "none",
+          "--media-button-icon-width": "100%",
+          width: "100%",
+          display: "flex",
+          flexFlow: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <MediaSeekBackwardButton
+          style={{ padding: 0, width: "20%" }}
+        ></MediaSeekBackwardButton>
+        <MediaPlayButton style={{ padding: 0, width: "20%" }}></MediaPlayButton>
+        <MediaSeekForwardButton
+          style={{ padding: 0, width: "20%" }}
+        ></MediaSeekForwardButton>
+      </div>
+      <MediaControlBar>
+        <MediaTimeRange></MediaTimeRange>
+        <MediaTimeDisplay show-duration remaining></MediaTimeDisplay>
+      </MediaControlBar>
+      <MediaControlBar>
+        <MediaMuteButton></MediaMuteButton>
+        <MediaVolumeRange></MediaVolumeRange>
+        <Spacer />
+        <MediaCaptionsButton></MediaCaptionsButton>
+        <MediaPipButton></MediaPipButton>
+        <MediaFullscreenButton></MediaFullscreenButton>
+        {hasAirPlay && <AirPlayButton onClick={onAirPlaySelected} />}
+      </MediaControlBar>
+    </>
+  );
+};
 
 export const VodChromeLarge: React.FC<ChromeProps> = (props) => {
   const { onAirPlaySelected, hasAirPlay = false } = props;
@@ -81,25 +181,46 @@ export const VodChromeLarge: React.FC<ChromeProps> = (props) => {
   );
 };
 
-export const LiveChromeSmall = () => {};
+export const LiveChromeSmall: React.FC<ChromeProps> = (props) => {
+  const { onAirPlaySelected, hasAirPlay = false } = props;
+  return (
+    <>
+      <div
+        slot="centered-chrome"
+        style={{
+          "--media-background-color": "none",
+          "--media-control-background": "none",
+          "--media-button-icon-width": "100%",
+          width: "100%",
+          display: "flex",
+          flexFlow: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <MediaPlayButton style={{ padding: 0, width: "20%" }}></MediaPlayButton>
+      </div>
+      <MediaControlBar>
+        <MediaMuteButton></MediaMuteButton>
+        <MediaVolumeRange></MediaVolumeRange>
+        <Spacer />
+        <MediaCaptionsButton></MediaCaptionsButton>
+        <MediaPipButton></MediaPipButton>
+        <MediaFullscreenButton></MediaFullscreenButton>
+        {hasAirPlay && <AirPlayButton onClick={onAirPlaySelected} />}
+      </MediaControlBar>
+    </>
+  );
+};
 export const LiveChromeLarge: React.FC<ChromeProps> = (props) => {
   const { onAirPlaySelected, hasAirPlay = false } = props;
   return (
     <>
-      {/* <div slot="centered-chrome">
-        <MediaSeekBackwardButton></MediaSeekBackwardButton>
-        <MediaPlayButton></MediaPlayButton>
-        <MediaSeekForwardButton></MediaSeekForwardButton>
-      </div> */}
-      {/* <MediaControlBar>
-        <MediaTimeRange></MediaTimeRange>
-        <MediaTimeDisplay show-duration remaining></MediaTimeDisplay>
-      </MediaControlBar> */}
       <MediaControlBar>
         <MediaPlayButton></MediaPlayButton>
         <MediaMuteButton></MediaMuteButton>
         <MediaVolumeRange></MediaVolumeRange>
-        <div style={{ flexGrow: 1 }}></div>
+        <Spacer />
         <MediaCaptionsButton></MediaCaptionsButton>
         <MediaPlaybackRateButton></MediaPlaybackRateButton>
         <MediaPipButton></MediaPipButton>
@@ -111,7 +232,13 @@ export const LiveChromeLarge: React.FC<ChromeProps> = (props) => {
 };
 
 export const DefaultChromeRenderer: React.FC<ChromeProps> = (props) => {
-  const { streamType } = props;
+  const { streamType, playerSize } = props;
+  if (playerSize === MediaChromeSizes.SM) {
+    if (streamType === StreamTypes.LIVE || streamType === StreamTypes.LL_LIVE) {
+      return <LiveChromeSmall {...props} />;
+    }
+    return <VodChromeSmall {...props} />;
+  }
   if (streamType === StreamTypes.LIVE || streamType === StreamTypes.LL_LIVE) {
     return <LiveChromeLarge {...props} />;
   }
@@ -148,17 +275,19 @@ const getChromeStylesFromProps = (props: MuxPlayerProps) => {
   const tertiaryColorStyles = tertiaryColor
     ? {
         "--media-range-track-background": tertiaryColor,
-        "background-color": tertiaryColor,
       }
     : {};
 
   return {
+    maxWidth: "100%",
     color: "#ffffff",
     ...primaryColorStyles,
     ...secondaryColorStyles,
     ...tertiaryColorStyles,
   };
 };
+
+const SMALL_BREAKPOINT = 700;
 
 export const MuxPlayer: React.FC<MuxPlayerProps> = (props) => {
   const {
@@ -175,6 +304,7 @@ export const MuxPlayer: React.FC<MuxPlayerProps> = (props) => {
     debug,
     ChromeRenderer = DefaultChromeRenderer,
   } = props;
+
   let supportsAirPlay = false;
   useEffect(() => {
     supportsAirPlay = !!window.WebKitPlaybackTargetAvailabilityEvent;
@@ -205,8 +335,24 @@ export const MuxPlayer: React.FC<MuxPlayerProps> = (props) => {
     );
   }, []);
 
+  const [playerSize, setPlayerSize] = useState(MediaChromeSizes.LG);
+  const [mediaControllerRect, mediaControllerRef] = useRect();
+  useEffect(() => {
+    if (!mediaControllerRect) return;
+
+    const nextPlayerSize =
+      mediaControllerRect.width < SMALL_BREAKPOINT
+        ? MediaChromeSizes.SM
+        : MediaChromeSizes.LG;
+
+    setPlayerSize(nextPlayerSize);
+  }, [mediaControllerRect]);
+
   return (
-    <MediaController style={getChromeStylesFromProps(props)}>
+    <MediaController
+      ref={mediaControllerRef}
+      style={getChromeStylesFromProps(props)}
+    >
       <MuxVideo
         key={playbackId}
         ref={muxVideoRefCb}
@@ -242,6 +388,7 @@ export const MuxPlayer: React.FC<MuxPlayerProps> = (props) => {
           muxVideoRef.current?.webkitShowPlaybackTargetPicker?.();
         }}
         streamType={streamType}
+        playerSize={playerSize}
       />
     </MediaController>
   );
