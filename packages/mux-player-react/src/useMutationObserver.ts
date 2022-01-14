c257573cbb5c9224bd36feb35fa26fa4b541d6cc
Christian Pillsbury <cjpillsbury@gmail.com>
2022-01-14 09:26:12 -0600

    feat/mux-player-react: cleanup sizing logic.

---
+++ packages/mux-player-react/src/useMutationObserver.ts
@@ -0,0 +1,44 @@
+// NOTE: This is pulled directly from rooks (https://github.com/imbhargav5/rooks/blob/main/src/hooks/useMutationObserver.ts)
+// Should add as a dependency as long as we can narrowly include all/only the hooks we need (or sufficient tree shaking) (CJP)
+
+import type { MutableRefObject } from "react";
+import { useEffect, Ref } from "react";
+
+const config: MutationObserverInit = {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+};
+
+/**
+ *
+ * useMutationObserver hook
+ *
+ * Returns a mutation observer for a React Ref and fires a callback
+ *
+ * @param {MutableRefObject<HTMLElement | null>} ref React ref on which mutations are to be observed
+ * @param {MutationCallback} callback Function that needs to be fired on mutation
+ * @param {MutationObserverInit} options
+ */
+function useMutationObserver(
+  ref: MutableRefObject<HTMLElement | null>,
+  callback: MutationCallback,
+  options: MutationObserverInit = config
+): void {
+  useEffect(() => {
+    // Create an observer instance linked to the callback function
+    if (ref.current) {
+      const observer = new MutationObserver(callback);
+
+      // Start observing the target node for configured mutations
+      observer.observe(ref.current, options);
+
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, [callback, options]);
+}
+
+export { useMutationObserver };
