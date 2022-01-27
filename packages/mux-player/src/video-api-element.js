3bc9d6bf8eb0ba84f1c8f45c1bdd81b3bb0df243
Wesley Luyten <me@wesleyluyten.com>
2022-01-26 21:42:09 -0600

    Add more native video functionality

--- packages/mux-player/src/video-api-element.js
+++ packages/mux-player/src/video-api-element.js
@@ -1,15 +1,28 @@
+const VideoAttributeNames = [
+  'autoplay',
+  'crossorigin',
+  'loop',
+  'muted',
+  'playsinline',
+  'src',
+];
+
 class VideoApiElement extends HTMLElement {
+  static get observedAttributes() {
+    return [...VideoAttributeNames];
+  }
+
   constructor() {
     super();
 
-    this.querySelectorAll(":scope > track").forEach((track) => {
+    this.querySelectorAll(':scope > track').forEach((track) => {
       this.video?.append(track.cloneNode());
     });
 
     // Watch for child adds/removes and update the native element if necessary
     const mutationCallback = (mutationsList) => {
       for (let mutation of mutationsList) {
-        if (mutation.type === "childList") {
+        if (mutation.type === 'childList') {
           // Child being removed
           mutation.removedNodes.forEach((node) => {
             const track = this.video?.querySelector(`track[src="${node.src}"]`);
@@ -27,9 +40,47 @@ class VideoApiElement extends HTMLElement {
     observer.observe(this, { childList: true, subtree: true });
   }
 
-  get video() {
-    return this.shadowRoot?.querySelector("mux-video");
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    if (VideoAttributeNames.includes(attrName)) {
+      this.video?.setAttribute(attrName, newValue);
+    }
   }
+
+  get video() {
+    return this.shadowRoot?.querySelector('mux-video');
+  }
+
+  get autoplay() {
+    return getVideoAttribute(this, 'autoplay') != null;
+  }
+
+  get crossOrigin() {
+    return getVideoAttribute(this, 'crossorigin');
+  }
+
+  get loop() {
+    return getVideoAttribute(this, 'loop') != null;
+  }
+
+  get muted() {
+    return getVideoAttribute(this, 'muted') != null;
+  }
+
+  get src() {
+    return getVideoAttribute(this, 'src');
+  }
+
+  set src(val) {
+    if (val == null) {
+      this.removeAttribute('src');
+    } else {
+      this.setAttribute('src', val);
+    }
+  }
+}
+
+function getVideoAttribute(el, name) {
+  return el.video ? el.video.getAttribute(name) : el.getAttribute(name);
 }
 
 export default VideoApiElement;
