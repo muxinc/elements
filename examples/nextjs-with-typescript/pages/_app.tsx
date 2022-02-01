80628a94c56a28780e71dccd03ae1645c032cbfa
Christian Pillsbury <cjpillsbury@gmail.com>
2022-02-01 08:31:20 -0600

    feat/mux-player-react: light lift application of mux-player css in nextjs example app.

---
+++ examples/nextjs-with-typescript/pages/_app.tsx
@@ -0,0 +1,6 @@
+import "@mux-elements/mux-player-react/dist/index.css";
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
