0eb1aa79116fb8051f985ad0078c3c7dc437a2da
Christian Pillsbury <cjpillsbury@gmail.com>
2022-02-15 10:54:17 -0600

    feat/mux-player-react: try to not have warnings explode CI builds.

--- examples/nextjs-with-typescript/pages/MuxPlayer.tsx
+++ examples/nextjs-with-typescript/pages/MuxPlayer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Link from "next/link";
 import MuxPlayer from "@mux-elements/mux-player-react";
 import { useRef, useState } from "react";
