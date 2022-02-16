f5eafb51e63ae0bbdfc7540cf0232f1b3b9b7360
Christian Pillsbury <cjpillsbury@gmail.com>
2022-02-16 07:58:09 -0600

    feat/mux-player-react: Update nextjs MuxPlayer example to use metadata and envKey.

--- examples/nextjs-with-typescript/pages/MuxPlayer.tsx
+++ examples/nextjs-with-typescript/pages/MuxPlayer.tsx
@@ -6,6 +6,12 @@ import { useRef, useState } from "react";
 const INITIAL_DEBUG = false;
 const INITIAL_MUTED = true;
 const INITIAL_PLAYBACK_ID = "g65IqSFtWdpGR100c2W8VUHrfIVWTNRen";
+const INITIAL_ENV_KEY = "5e67cqdt7hgc9vkla7p0qch7q";
+const INITIAL_METADATA = {
+  "metadata-video-id": "video-id-bc789",
+  "metadata-video-title": "Great Stuff",
+  "metadata-viewer-user-id": "user-id-6af7",
+};
 
 const onLoadStart = console.log.bind(null, "loadstart");
 const onLoadedMetadata = console.log.bind(null, "loadedmetadata");
@@ -28,6 +34,8 @@ const onPlayerReady = console.log.bind(null, "playerready");
 function MuxPlayerPage() {
   const mediaElRef = useRef(null);
   const [playbackId, setPlaybackId] = useState(INITIAL_PLAYBACK_ID);
+  const [envKey, setEnvKey] = useState(INITIAL_ENV_KEY);
+  const [metadata, _setMetadata] = useState(INITIAL_METADATA);
   const [muted, setMuted] = useState(INITIAL_MUTED);
   const [debug, setDebug] = useState(INITIAL_DEBUG);
   return (
@@ -44,6 +52,8 @@ function MuxPlayerPage() {
         <MuxPlayer
           ref={mediaElRef}
           // style={{ aspectRatio: "16 / 9" }}
+          envKey={envKey}
+          metadata={metadata}
           playbackId={playbackId}
           forwardSeekOffset={10}
           backwardSeekOffset={10}
@@ -51,7 +61,7 @@ function MuxPlayerPage() {
           debug={debug}
           muted={muted}
           autoPlay
-          streamType={"live"}
+          // streamType={"live"}
           primaryColor="#ec407a"
           secondaryColor="#64b5f6"
           tertiaryColor="#b4004e"
@@ -87,6 +97,14 @@ function MuxPlayerPage() {
             id="playback-id-control"
             onBlur={({ currentTarget }) => setPlaybackId(currentTarget.value)}
             defaultValue={playbackId}
+          />
+        </div>
+        <div>
+          <label htmlFor="env-key-control">Env Key (Mux Data)</label>
+          <input
+            id="env-key-control"
+            onBlur={({ currentTarget }) => setEnvKey(currentTarget.value)}
+            defaultValue={envKey}
           />
         </div>
       </div>
