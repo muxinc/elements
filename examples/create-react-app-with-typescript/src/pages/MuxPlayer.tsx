7a0b7c1e60bfeb6528a518835899b11436d13297
Christian Pillsbury <cjpillsbury@gmail.com>
2022-02-16 08:43:46 -0600

    feat/mux-player-react: Update CRA and next.js examples for metadata and paused.

--- examples/create-react-app-with-typescript/src/pages/MuxPlayer.tsx
+++ examples/create-react-app-with-typescript/src/pages/MuxPlayer.tsx
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 import MuxPlayer from "@mux-elements/mux-player-react";
 import { useRef, useState } from "react";
 
-// const INITIAL_DEBUG = true;
 const INITIAL_DEBUG = false;
 const INITIAL_MUTED = true;
 const INITIAL_PLAYBACK_ID = "g65IqSFtWdpGR100c2W8VUHrfIVWTNRen";
+const INITIAL_ENV_KEY = "5e67cqdt7hgc9vkla7p0qch7q";
+const INITIAL_METADATA = {
+  "metadata-video-id": "video-id-bc789",
+  "metadata-video-title": "Great Stuff",
+  "metadata-viewer-user-id": "user-id-6af7",
+};
 
 const onLoadStart = console.log.bind(null, "loadstart");
 const onLoadedMetadata = console.log.bind(null, "loadedmetadata");
@@ -29,6 +34,9 @@ const onPlayerReady = console.log.bind(null, "playerready");
 function MuxPlayerPage() {
   const mediaElRef = useRef(null);
   const [playbackId, setPlaybackId] = useState(INITIAL_PLAYBACK_ID);
+  const [envKey, setEnvKey] = useState(INITIAL_ENV_KEY);
+  const [metadata, _setMetadata] = useState(INITIAL_METADATA);
+  const [paused, setPaused] = useState<boolean | undefined>(false);
   const [muted, setMuted] = useState(INITIAL_MUTED);
   const [debug, setDebug] = useState(INITIAL_DEBUG);
   return (
@@ -45,25 +53,43 @@ function MuxPlayerPage() {
         <MuxPlayer
           ref={mediaElRef}
           // style={{ aspectRatio: "16 / 9" }}
+          envKey={envKey}
+          metadata={metadata}
           playbackId={playbackId}
           forwardSeekOffset={10}
           backwardSeekOffset={10}
           // onPlayerReady={() => console.log("ready!")}
           debug={debug}
           muted={muted}
+          paused={paused}
           autoPlay
-          streamType={"live"}
+          // streamType={"live"}
           primaryColor="#ec407a"
           secondaryColor="#64b5f6"
           tertiaryColor="#b4004e"
-          onPlay={onPlay}
-          onPause={onPause}
+          onPlay={(evt) => {
+            onPlay(evt);
+            setPaused(false);
+          }}
+          onPause={(evt) => {
+            onPause(evt);
+            setPaused(true);
+          }}
           onSeeking={onSeeking}
           onSeeked={onSeeked}
           // startTime={12}
         />
       </div>
       <div>
+        <div>
+          <label htmlFor="paused-control">Paused</label>
+          <input
+            id="paused-control"
+            type="checkbox"
+            onChange={() => setPaused(!paused)}
+            checked={paused}
+          />
+        </div>
         <div>
           <label htmlFor="muted-control">Muted</label>
           <input
@@ -88,6 +114,14 @@ function MuxPlayerPage() {
             id="playback-id-control"
             onBlur={({ currentTarget }) => setPlaybackId(currentTarget.value)}
             defaultValue={playbackId}
+          />
+        </div>
+        <div>
+          <label htmlFor="env-key-control">Env Key (Mux Data)</label>
+          <input
+            id="env-key-control"
+            onBlur={({ currentTarget }) => setEnvKey(currentTarget.value)}
+            defaultValue={envKey}
           />
         </div>
       </div>
