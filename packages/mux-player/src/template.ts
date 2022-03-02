5f8f2d4e45dff2b448b2c303e83c23d3990eaad0
Wesley Luyten <me@wesleyluyten.com>
2022-03-02 11:23:48 -0600

    Fix tests

--- packages/mux-player/src/template.ts
+++ packages/mux-player/src/template.ts
@@ -38,10 +38,10 @@ export const template = (props: MuxTemplateProps) => html`
       slot="media"
       crossorigin
       playsinline
-      autoplay="${props.autoplay}"
-      muted="${props.muted}"
-      debug="${props.debug}"
-      prefer-mse="${props.preferMse}"
+      autoplay="${props.autoplay ?? false}"
+      muted="${props.muted ?? false}"
+      debug="${props.debug ?? false}"
+      prefer-mse="${props.preferMse ?? false}"
       start-time="${props.startTime != null ? props.startTime : false}"
       src="${!!props.src
         ? props.src
