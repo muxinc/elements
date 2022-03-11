62e2e6329ad1c7e754773ff2832460ca6f2302fb
Christian Pillsbury <cjpillsbury@gmail.com>
2022-03-11 14:00:37 -0600

    fix(mux-player): Add support for event handlers to template parts processing.

--- packages/mux-player/src/html.ts
+++ packages/mux-player/src/html.ts
@@ -6,6 +6,49 @@ import {
   AttributeTemplatePart,
 } from "@github/template-parts";
 import type { TemplatePart, TemplateTypeInit } from "@github/template-parts";
+// NOTE: These are either direct ports or significantly based off of github's jtml template part processing logic. For more, see: https://github.com/github/jtml
+
+const eventListeners = new WeakMap<Element, Map<string, EventHandler>>();
+class EventHandler {
+  handleEvent!: EventListener;
+  constructor(private element: Element, private type: string) {
+    this.element.addEventListener(this.type, this);
+    eventListeners.get(this.element)!.set(this.type, this);
+  }
+  set(listener: EventListener) {
+    if (typeof listener == "function") {
+      this.handleEvent = listener.bind(this.element);
+    } else if (
+      typeof listener === "object" &&
+      typeof (listener as EventHandler).handleEvent === "function"
+    ) {
+      this.handleEvent = (listener as EventHandler).handleEvent.bind(listener);
+    } else {
+      this.element.removeEventListener(this.type, this);
+      eventListeners.get(this.element)!.delete(this.type);
+    }
+  }
+  static for(part: AttributeTemplatePart): EventHandler {
+    if (!eventListeners.has(part.element))
+      eventListeners.set(part.element, new Map());
+    const type = part.attributeName.slice(2);
+    const elementListeners = eventListeners.get(part.element)!;
+    if (elementListeners.has(type)) return elementListeners.get(type)!;
+    return new EventHandler(part.element, type);
+  }
+}
+
+export function processEvent(part: TemplatePart, value: unknown): boolean {
+  if (
+    part instanceof AttributeTemplatePart &&
+    part.attributeName.startsWith("on")
+  ) {
+    EventHandler.for(part).set(value as unknown as EventListener);
+    part.element.removeAttributeNS(part.attributeNamespace, part.attributeName);
+    return true;
+  }
+  return false;
+}
 
 function processSubTemplate(part: TemplatePart, value: unknown): boolean {
   if (value instanceof TemplateResult && part instanceof NodeTemplatePart) {
@@ -72,6 +115,7 @@ export function processBooleanNode(
 
 export function processPart(part: TemplatePart, value: unknown): void {
   processBooleanAttribute(part, value) ||
+    processEvent(part, value) ||
     processBooleanNode(part, value) ||
     processSubTemplate(part, value) ||
     processDocumentFragment(part, value) ||
