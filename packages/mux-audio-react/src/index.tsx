1b179f28cbc27f913aabf4cda2d05882a5e82512
Christian Pillsbury <cjpillsbury@gmail.com>
2022-03-14 07:22:56 -0500

    fix(mux-audio-react): use generateInitTime() instead of Date.now.

--- packages/mux-audio-react/src/index.tsx
+++ packages/mux-audio-react/src/index.tsx
@@ -11,6 +11,7 @@ import {
   StreamTypes,
   toMuxVideoURL,
   PlaybackEngine,
+  generatePlayerInitTime,
 } from "@mux-elements/playback-core";
 import { getPlayerVersion } from "./env";
 
@@ -43,7 +44,7 @@ const MuxAudio = React.forwardRef<HTMLAudioElement | undefined, Partial<Props>>(
       ...restProps
     } = props;
 
-    const [playerInitTime] = useState(Date.now());
+    const [playerInitTime] = useState(generatePlayerInitTime());
 
     const [src, setSrc] = useState<MuxMediaProps["src"]>(
       toMuxVideoURL(playbackId) ?? outerSrc
