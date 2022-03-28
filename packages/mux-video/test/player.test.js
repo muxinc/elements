7e0cf64220f71b0d10fee16f7e355ee239a05061
Gary Katsevman <git@gkatsev.com>
2022-03-28 11:44:53 -0400

    add some tests

--- packages/mux-video/test/player.test.js
+++ packages/mux-video/test/player.test.js
@@ -20,4 +20,65 @@ describe('<mux-video>', () => {
     assert.equal(player.preferMse, true, 'prefer-mse is on');
     assert.equal(player.debug, true, 'debug is on');
   });
+
+  it('can use extended autoplay properties on initial load', async function () {
+    const player = await fixture(`<mux-video
+      autoplay="muted"
+    ></mux-video>`);
+
+    assert.equal(player.autoplay, 'muted', 'our autoplay setting is muted');
+  });
+
+  it('can use extended autoplay properties', async function () {
+    const player = await fixture(`<mux-video
+    ></mux-video>`);
+
+    assert.equal(player.autoplay, false, 'initial autoplay is false');
+
+    player.autoplay = 'muted';
+
+    assert.equal(player.autoplay, 'muted', 'can set autoplay to "muted"');
+    assert.equal(player.getAttribute('autoplay'), 'muted', 'the attribute is set to "muted"');
+
+    player.autoplay = 'any';
+
+    assert.equal(player.autoplay, 'any', 'can set autoplay to "any"');
+    assert.equal(player.getAttribute('autoplay'), 'any', 'the attribute is set to "muted"');
+
+    player.autoplay = false;
+
+    assert.isFalse(player.autoplay, 'can turn off autoplay');
+    assert.isFalse(player.hasAttribute('autoplay'), 'setting to false reomves the attribute');
+
+    player.autoplay = true;
+    assert.isTrue(player.autoplay, 'can turn off autoplay');
+    assert.equal(player.getAttribute('autoplay'), '', 'when prop is set to true, attribute value is an empty string');
+  });
+
+  it('can use extended autoplay attributes', async function () {
+    const player = await fixture(`<mux-video
+    ></mux-video>`);
+
+    assert.equal(player.autoplay, false, 'initial autoplay is false');
+
+    player.setAttribute('autoplay', 'muted');
+
+    assert.equal(player.autoplay, 'muted', 'can set autoplay to "muted"');
+    assert.equal(player.getAttribute('autoplay'), 'muted', 'the attribute is set to "muted"');
+
+    player.setAttribute('autoplay', 'any');
+
+    assert.equal(player.autoplay, 'any', 'can set autoplay to "any"');
+    assert.equal(player.getAttribute('autoplay'), 'any', 'the attribute is set to "muted"');
+
+    player.removeAttribute('autoplay');
+    player.autoplay = false;
+
+    assert.isFalse(player.autoplay, 'can turn off autoplay');
+    assert.isFalse(player.hasAttribute('autoplay'), 'setting to false reomves the attribute');
+
+    player.setAttribute('autoplay', '');
+    assert.isTrue(player.autoplay, 'can turn off autoplay');
+    assert.equal(player.getAttribute('autoplay'), '', 'when prop is set to true, attribute value is an empty string');
+  });
 });
