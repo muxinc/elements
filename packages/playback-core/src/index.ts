3a1941ca2202a0d542cb5fae300214620f80088e
Wesley Luyten <me@wesleyluyten.com>
2022-04-15 12:53:14 -0500

    chore: exclude decode errors from src fetch

--- packages/playback-core/src/index.ts
+++ packages/playback-core/src/index.ts
@@ -362,8 +362,8 @@ async function handleNativeError(event: Event) {
   const { message, code } = mediaEl?.error ?? {};
   const error = new MediaError(message, code);
 
-  if (mediaEl.src) {
-    // Attempt to get the response code from the M3U8 src url.
+  if (mediaEl.src && (code !== MediaError.MEDIA_ERR_DECODE || code !== undefined)) {
+    // Attempt to get the response code from the video src url.
     const { status } = await fetch(mediaEl.src as RequestInfo);
     // Use the same hls.js data structure.
     error.data = { response: { code: status } };
