2e54596995d3ca8ccdd505f846f0622561f7d709
clearlythuydoan <clearlythuydoan@gmail.com>
2022-06-30 16:33:30 -0400

    feat(mux-uploader): React wrapper for mux-uploader and mux-uploader-drop.

---
+++ packages/mux-uploader-react/src/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import '@mux/mux-uploader';
+import MuxUploaderDrop from './mux-uploader-drop';
+import type MuxUploaderElement from '@mux/mux-uploader';
+import { toNativeProps } from './common/utils';
+import { useRef } from 'react';
+import { useCombinedRefs } from './useCombinedRefs';
+
+export type MuxUploaderRefAttributes = MuxUploaderElement;
+
+export type MuxUploaderProps = {
+  url: string;
+  id?: string;
+  type?: string;
+  uploadInProgress?: boolean;
+  uploadError?: boolean;
+  status?: boolean;
+  disableDrop?: boolean;
+  children?: React.ReactNode;
+};
+
+const MuxUploaderInternal = React.forwardRef<MuxUploaderRefAttributes, MuxUploaderProps>(
+  ({ children, ...props }, ref) => {
+    return React.createElement('mux-uploader', toNativeProps({ ...props, ref }), children);
+  }
+);
+
+const useUploader = (
+  ref: // | ((instance: EventTarget | null) => void)
+  React.MutableRefObject<MuxUploaderElement | null> | null | undefined,
+  props: MuxUploaderProps
+) => {
+  const { url, id, type, uploadInProgress, uploadError, status, disableDrop, ...remainingProps } = props;
+
+  return [remainingProps];
+};
+
+const MuxUploader = React.forwardRef<MuxUploaderRefAttributes, MuxUploaderProps>((props, ref) => {
+  const { url, id, type, uploadInProgress, uploadError, status, disableDrop } = props;
+
+  const innerUploaderRef = useRef<MuxUploaderElement>(null);
+  const uploaderRef = useCombinedRefs(innerUploaderRef, ref);
+  const [remainingProps] = useUploader(innerUploaderRef, props);
+
+  return (
+    <MuxUploaderInternal
+      ref={uploaderRef as typeof innerUploaderRef}
+      url={url}
+      id={id}
+      type={type}
+      uploadInProgress={uploadInProgress}
+      uploadError={uploadError}
+      status={status}
+      disableDrop={disableDrop}
+      {...remainingProps}
+    />
+  );
+});
+
+export { MuxUploaderDrop };
+
+export default MuxUploader;
