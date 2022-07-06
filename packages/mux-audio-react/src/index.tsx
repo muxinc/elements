abf4fa254d8ccd65e286205f3b431df867a9c3fc
Gary Katsevman <git@gkatsev.com>
2022-07-06 19:57:05 -0400

    fix: add scope deprecation warning

--- packages/mux-audio-react/src/index.tsx
+++ packages/mux-audio-react/src/index.tsx
@@ -21,6 +21,11 @@ export type Props = Omit<
 > &
   MuxMediaProps;
 
+console.warn(`
+We have recently transitioned the package name from @mux-elements/<element name> to @mux/<element name>.
+Please update your imports or scripts. See #_#_INSERT_URL_HERE_#_# for more information.
+`);
+
 const playerSoftwareVersion = getPlayerVersion();
 const playerSoftwareName = 'mux-audio-react';
 
