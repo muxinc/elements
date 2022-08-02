5c05af8c257806662bc6402baba03b7090cbe699
Christian Pillsbury <cjpillsbury@gmail.com>
2022-08-02 08:24:46 -0700

    fix(mux-player): Account for attr vs. 'prop' naming overlap in state propogation.

--- packages/mux-player/src/template.ts
+++ packages/mux-player/src/template.ts
@@ -26,7 +26,7 @@ const forwardUniqueCSSParts = [...new Set(flatCSSParts)].join(', ');
 
 export const content = (props: MuxTemplateProps) => html`
   <${unsafeStatic(castThemeName(props.theme) ?? 'media-theme-mux')}
-    nohotkeys="${props.nohotkeys || false}"
+    nohotkeys="${props.noHotKeys ?? false}"
     audio="${props.audio || false}"
     style="${
       stylePropsToString({
