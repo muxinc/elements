0f511d43474454b683dd7e041bdc3a99937a936e
Wesley Luyten <me@wesleyluyten.com>
2022-09-08 15:01:47 -0400

    fix: startTime on iOS when preload=auto (#368)

--- packages/playback-core/src/util.ts
+++ packages/playback-core/src/util.ts
@@ -4,3 +4,15 @@ type KeyTypes = string | number | symbol;
 export const isKeyOf = <T = any>(k: KeyTypes, o: T): k is keyof T => {
   return k in o;
 };
+
+export function inSeekableRange(seekable: TimeRanges, duration: number, time: number) {
+  if (duration && time > duration) {
+    time = duration;
+  }
+  for (let i = 0; i < seekable.length ?? 0; i++) {
+    if (seekable.start(i) <= time && seekable.end(i) >= time) {
+      return true;
+    }
+  }
+  return false;
+}
