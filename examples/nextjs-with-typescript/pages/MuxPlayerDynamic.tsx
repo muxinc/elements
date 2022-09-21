38511360b86867f071a29148bc1527fe98fab875
Christian Pillsbury <cpillsbury@mux.com>
2022-09-21 11:15:53 -0700

    docs: temporary suboptimal fix for dynamic muxplayer nextjs example.

--- examples/nextjs-with-typescript/pages/MuxPlayerDynamic.tsx
+++ examples/nextjs-with-typescript/pages/MuxPlayerDynamic.tsx
@@ -1,7 +1,18 @@
+import type { GetServerSideProps } from "next";
 import dynamic from "next/dynamic";
 const MuxPlayerPageStatic = dynamic(() => import("./MuxPlayer"));
-function MuxPlayerPage() {
-  return <MuxPlayerPageStatic />;
+
+type Props = { location?: Pick<Location, 'origin' | 'pathname'> };
+export const getServerSideProps: GetServerSideProps<Props> = async context => {
+  console.log('context.req.headers', JSON.stringify(context.req.headers, null, 2));
+  const { origin, pathname }: Pick<Location, 'origin' | 'pathname'> = new URL(context.req.headers.referer ?? 'http://fake.com');
+  const location = { origin, pathname };
+  return ({ props: { location } })
+};
+
+
+function MuxPlayerPage({ location }: Props) {
+  return <MuxPlayerPageStatic location={location} />;
 }
 
 export default MuxPlayerPage;
