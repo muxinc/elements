9a559841412b73d0f970e530dca78bdec3c75b03
Wesley Luyten <wesley@mux.com>
2022-09-21 15:57:06 -0500

    docs: add Mux styles to Create React app

--- examples/create-react-app-with-typescript/src/pages/MuxUploader.tsx
+++ examples/create-react-app-with-typescript/src/pages/MuxUploader.tsx
@@ -11,29 +11,24 @@ function MuxUploaderPage() {
   }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexFlow: "column",
-        height: "100%",
-        width: "100%",
-      }}
-    >
-      <h1>MuxUploader Demo</h1>
+    <>
       <h2>Enter your upload GCS url:</h2>
-      <input type="text" style={{ padding: "8px 12px", marginBottom: "20px", width: "400px" }} placeholder="https://storage.googleapis.com/..." onChange={handleChange} />
-     
-      <div style={{ flexGrow: 1, flexShrink: 1, height: "400px" }}>
-        <MuxUploader
-          endpoint={url}
-          type="bar"
-          status
-        />
-      </div>
-      <h3 className="title">
-        <Link to="/">Browse Elements</Link>
-      </h3>
-    </div>
+
+      <input type="text" style={{
+        marginBottom: "20px",
+        width: "min(100%, 400px)",
+        boxSizing: "border-box"
+      }} placeholder="https://storage.googleapis.com/..." onChange={handleChange} />
+
+      <MuxUploader
+        style={{ display: "block", height: "200px" }}
+        endpoint={url}
+        type="bar"
+        status
+      />
+
+      <Link to="/">Browse Elements</Link>
+    </>
   );
 }
 
--- examples/create-react-app-with-typescript/src/pages/MuxVideo.tsx
+++ examples/create-react-app-with-typescript/src/pages/MuxVideo.tsx
@@ -3,35 +3,32 @@ import MuxVideo from "@mux/mux-video-react";
 
 function MuxVideoPage() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexFlow: "column",
-        height: "100%",
-        width: "100%",
-      }}
-    >
-      <h1>MuxVideo Demo</h1>
-      <div style={{ flexGrow: 1, flexShrink: 1, height: "400px" }}>
-        <MuxVideo
-          style={{ height: "100%", maxWidth: "100%" }}
-          playbackId="DS00Spx1CV902MCtPj5WknGlR102V5HFkDe"
-          // metadata={{
-          //   video_id: "video-id-12345",
-          //   video_title: "Mad Max: Fury Road Trailer",
-          //   viewer_user_id: "user-id-6789",
-          // }}
-          // envKey="mux-data-env-key"
-          streamType="on-demand"
-          controls
-          autoPlay
-          muted
-        />
-      </div>
-      <h3 className="title">
-        <Link to="/">Browse Elements</Link>
-      </h3>
-    </div>
+    <>
+      <style>
+        {`video {
+          display: block;
+          width: 100%;
+          aspect-ratio: 16 / 9;
+          margin: 1rem 0 2rem;
+        }`}
+      </style>
+
+      <MuxVideo
+        playbackId="a4nOgmxGWg6gULfcBbAa00gXyfcwPnAFldF8RdsNyk8M"
+        preload="auto"
+        startTime={4.6}
+        metadata={{
+          video_id: "video-id-12345",
+          video_title: "Star Wars: Episode 3",
+          viewer_user_id: "user-id-6789",
+        }}
+        streamType="on-demand"
+        controls
+        muted
+      />
+
+      <Link to="/">Browse Elements</Link>
+    </>
   );
 }
 
