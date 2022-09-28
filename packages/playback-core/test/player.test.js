4d53cde9ebdc43506dd08ce1044aa54e65ba87cd
Wesley Luyten <wesley@mux.com>
2022-09-28 14:22:45 -0500

    test: add playback core tests

---
+++ packages/playback-core/test/player.test.js
@@ -0,0 +1,147 @@
+import { assert, aTimeout } from '@open-wc/testing';
+import { initialize, teardown } from '../src/index.ts';
+
+describe('playback core', () => {
+  it('initialize w/ default preferPlayback', async function () {
+    const video = document.createElement('video');
+    const core = initialize(
+      {
+        src: 'https://stream.mux.com/23s11nz72DsoN657h4314PjKKjsF2JG33eBQQt6B95I.m3u8',
+      },
+      video
+    );
+    assert.equal(typeof core, 'object');
+    assert.equal(typeof core.engine, 'object');
+  });
+
+  it('teardown', async function () {
+    const video = document.createElement('video');
+    const core = initialize(
+      {
+        src: 'https://stream.mux.com/23s11nz72DsoN657h4314PjKKjsF2JG33eBQQt6B95I.m3u8',
+      },
+      video
+    );
+    teardown(video, core);
+
+    assert(!video.hasAttribute('src'), 'src is removed on teardown');
+  });
+
+  it('setAutoplay("any")', async function () {
+    this.timeout(10000);
+
+    const video = document.createElement('video');
+    const core = initialize(
+      {
+        src: 'https://stream.mux.com/23s11nz72DsoN657h4314PjKKjsF2JG33eBQQt6B95I.m3u8',
+        autoplay: false,
+      },
+      video
+    );
+
+    core.setAutoplay('any');
+    await new Promise((resolve) => video.addEventListener('playing', resolve));
+
+    assert(!video.paused, 'is playing after core.setAutoplay("any")');
+  });
+
+  it('setAutoplay("muted")', async function () {
+    this.timeout(10000);
+
+    const video = document.createElement('video');
+    const core = initialize(
+      {
+        src: 'https://stream.mux.com/23s11nz72DsoN657h4314PjKKjsF2JG33eBQQt6B95I.m3u8',
+        autoplay: false,
+      },
+      video
+    );
+
+    core.setAutoplay('muted');
+    await new Promise((resolve) => video.addEventListener('playing', resolve));
+
+    assert(!video.paused, 'is playing after core.setAutoplay("muted")');
+    assert(video.muted, 'video is muted for autoplaying');
+  });
+
+  it('preload="none"', async function () {
+    this.timeout(10000);
+
+    const video = document.createElement('video');
+    initialize(
+      {
+        src: 'https://stream.mux.com/23s11nz72DsoN657h4314PjKKjsF2JG33eBQQt6B95I.m3u8',
+        preload: 'none',
+      },
+      video
+    );
+
+    assert.equal(video.preload, 'none', 'preload is none');
+    await aTimeout(3000);
+    assert.equal(video.buffered.length, 0, 'no buffer loaded');
+
+    video.muted = true;
+    try {
+      await video.play();
+    } catch (error) {
+      console.warn(error);
+    }
+
+    assert(!video.paused, 'is playing after play()');
+  });
+
+  it('preload="auto"', async function () {
+    this.timeout(10000);
+
+    const video = document.createElement('video');
+    initialize(
+      {
+        src: 'https://stream.mux.com/23s11nz72DsoN657h4314PjKKjsF2JG33eBQQt6B95I.m3u8',
+        preload: 'auto',
+      },
+      video
+    );
+
+    assert.equal(video.preload, 'auto', 'preload is auto');
+    await new Promise((resolve) => video.addEventListener('progress', resolve));
+    assert.equal(video.buffered.length, 1, 'some buffer loaded');
+
+    video.muted = true;
+    try {
+      await video.play();
+    } catch (error) {
+      console.warn(error);
+    }
+
+    assert(!video.paused, 'is playing after play()');
+  });
+
+  it('preload="none" to preload="metadata"', async function () {
+    this.timeout(10000);
+
+    const video = document.createElement('video');
+    const core = initialize(
+      {
+        src: 'https://stream.mux.com/23s11nz72DsoN657h4314PjKKjsF2JG33eBQQt6B95I.m3u8',
+        preload: 'none',
+      },
+      video
+    );
+
+    assert.equal(video.preload, 'none', 'preload is none');
+
+    core.setPreload('metadata');
+    assert.equal(video.preload, 'metadata', 'preload is metadata');
+    await new Promise((resolve) => video.addEventListener('progress', resolve));
+    assert.equal(video.buffered.length, 1, 'some buffer loaded');
+
+    video.muted = true;
+    try {
+      await video.play();
+    } catch (error) {
+      console.warn(error);
+    }
+
+    assert(!video.paused, 'is playing after play()');
+  });
+});
---
+++ packages/playback-core/test/web-test-runner.config.mjs
@@ -0,0 +1,29 @@
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { importMapsPlugin } from '@web/dev-server-import-maps';
+
+export default {
+  nodeResolve: true,
+  plugins: [
+    importMapsPlugin({
+      inject: {
+        importMap: {
+          imports: {
+            // see shared/test-esm-exports/README.md for more information on this configuration
+            '/test/': '/packages/mux-video/test/',
+            'hls.js': '/node_modules/@mux/test-esm-exports/dist/hls.js',
+            'mux-embed': '/node_modules/@mux/test-esm-exports/dist/mux-embed.js',
+          },
+        },
+      },
+    }),
+    esbuildPlugin({
+      ts: true,
+      json: true,
+      loaders: { '.css': 'text', '.svg': 'text' },
+    }),
+  ],
+  coverageConfig: {
+    report: true,
+    include: ['src/**/*'],
+  },
+};
