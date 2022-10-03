20dafb752c9dca7e45fe9e4680e75102283f3c72
Darius Cepulis <dcepulis@mux.com>
2022-10-03 16:22:41 -0500

    Spin spinner back into lazy

--- packages/mux-player-react/src/lazy.tsx
+++ packages/mux-player-react/src/lazy.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-import Spinner from './Spinner';
 import ConditionalSuspense from './ConditionalSuspense';
 import useIsBrowser from './useIsBrowser';
 import useIsIntersecting from './useIsIntersecting';
@@ -52,7 +51,7 @@ const Fallback = (props: FallbackProps) => {
         {...rest}
       >
         {/* Loading Spinner SVG */}
-        <Spinner
+        <svg
           aria-hidden="true"
           style={{
             position: 'absolute',
@@ -62,7 +61,24 @@ const Fallback = (props: FallbackProps) => {
             height: '100px',
             transform: 'translate(-50%,-50%)',
           }}
-        />
+          viewBox="0 0 100 100"
+          fill="transparent"
+        >
+          <path d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+            {/* after half a second, animate fill to appear */}
+            <animate attributeName="fill" dur="0.1s" from="transparent" to="#fff" begin="0.5s" fill="freeze" />
+            {/* spin! */}
+            <animateTransform
+              attributeName="transform"
+              attributeType="XML"
+              type="rotate"
+              dur="1s"
+              from="0 50 50"
+              to="360 50 50"
+              repeatCount="indefinite"
+            />
+          </path>
+        </svg>
         {/* Overlay */}
         <div
           // TODO: this color isn't quite right. how does mux player get its dark backdrop?
