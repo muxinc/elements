08d25b8fb7df7c0bd2ddb186fbe7914ee333f6ea
Gaston Bogarin <gastonb@qualabs.com>
2022-11-09 12:35:08 -0300

    Add links to summary

--- .github/external-testing/createSummary.js
+++ .github/external-testing/createSummary.js
@@ -1,35 +1,52 @@
 module.exports = async ({core}, data) => {
+  if(data.Reference)
+      convertToArray({core}, data.Reference, null, null);
   
-    const keys = Object.keys(data);
-    keys.forEach(async a =>{
-      if(a.includes("Summary")){
-        const suiteName = a.split("_")[1];
-        const browser = a.split("_")[2];
-        await convertToArray({core}, data[a], suiteName, browser);
-      }
-    });
-    
-    if(data.Reference)
-      await convertToArray({core}, data.Reference, null, null);
-    
-    await core.summary.write();
-  }
-  
-  async function convertToArray({core}, data, suiteName, browser){
-    if(!data)
-       return;
-    
-    data = data.replaceAll('[', '');
-    data = data.replaceAll('"', '');
-    const array = data.split("],");
-    for (i=0;i<array.length;i++) { 
-      array[i] = array[i].replaceAll("]]", "");
-      array[i] = array[i].split(",");
+  let keys = Object.keys(data);
+  keys.forEach(a =>{
+    if(a.includes("Summary")){
+      const suiteName = a.split("_")[1];
+      const browser = a.split("_")[2];
+      convertToArray({core}, data[a], suiteName, browser);
+      const linkData = a.replaceAll("Summary", "Links");
+      addLinks({core}, data[linkData]);
     }
-    
-     const header = suiteName ? `${browser} - ${suiteName}` : "References";
-      
-    await core.summary
-    .addHeading(header, 3)
-    .addTable(array);
+  });
+  
+  await core.summary.write();
+}
+
+function addLinks({core}, data){
+  console.log(data);
+  if(!data)
+     return;
+  data = data.replaceAll('[', '');
+  data = data.replaceAll('"', '');
+  const array = data.split("],");
+  for (i=0;i<array.length;i++) { 
+    array[i] = array[i].replaceAll("]]", "");
+    array[i] = array[i].split(",");
   }
+    
+  for (i=0;i<array.length;i++) { 
+      core.summary.addLink(array[i][0], array[i][1]);
+  }
+  
+}
+
+function convertToArray({core}, data, suiteName, browser){
+  if(!data)
+     return;
+  
+  data = data.replaceAll('[', '');
+  data = data.replaceAll('"', '');
+  const array = data.split("],");
+  for (i=0;i<array.length;i++) { 
+    array[i] = array[i].replaceAll("]]", "");
+    array[i] = array[i].split(",");
+  }
+  
+  const header = suiteName ? `${browser} - ${suiteName}` : "References";
+    
+  core.summary.addHeading(header,3).addTable(array);
+}
