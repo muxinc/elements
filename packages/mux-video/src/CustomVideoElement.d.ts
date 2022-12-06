b1daf98b34ce875c90490a8dff9f7cc9f0b4df76
Christian Pillsbury <cpillsbury@mux.com>
2022-12-06 08:39:18 -0800

    fix(mux-video): Clean up some type defs.

--- packages/mux-video/src/CustomVideoElement.d.ts
+++ packages/mux-video/src/CustomVideoElement.d.ts
@@ -1,17 +1,14 @@
-import { VideoEvents } from './CustomVideoElement.js';
-
 /** @TODO Add type defs to custom-video-element directly */
+type HTMLVideoElementWithoutOverrides = Omit<HTMLVideoElement, 'autoplay'>;
 export default class CustomVideoElement<V extends HTMLVideoElement = HTMLVideoElement>
   // NOTE: "lying" here since we programmatically merge props/methods from HTMLVideoElement into the CustomVideoElement's prototype in an attempt to make it "look like"
   // it extends an HTMLVideoElement.
   extends HTMLVideoElement
-  implements HTMLVideoElement
+  implements HTMLVideoElementWithoutOverrides
 {
   static readonly observedAttributes: Array<string>;
   readonly nativeEl: V;
   attributeChangedCallback(attrName: string, oldValue?: string | null, newValue?: string | null): void;
-  get autoplay(): string | boolean;
-  set autoplay(): string | boolean;
 }
 
-export const VideoEvents: string[] = VideoEvents;
+export declare const VideoEvents: string[];
