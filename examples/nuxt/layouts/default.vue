284c10ea05e44ee703c11587fc50e005ea331672
Wesley Luyten <wesley@mux.com>
2022-12-07 13:10:12 -0600

    chore: add Nuxt to examples

---
+++ examples/nuxt/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <Head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <link rel="stylesheet" href="/base.css">
+  </Head>
+
+  <header>
+    <div class="left-header">
+      <a class="mux-logo" href="https://www.mux.com/player" target="_blank">
+        <img width="81" height="26" src="/images/mux-logo@2x.webp" alt="Mux logo" decoding="async" />
+      </a>
+      <h1><RouterLink to="/">Elements</RouterLink></h1>
+    </div>
+    <div class="right-header">
+      <a class="github-logo" href="https://github.com/muxinc/elements" target="_blank">
+        <img width="32" height="32" src="/images/github-logo.svg" alt="Github logo" />
+      </a>
+    </div>
+  </header>
+
+  <slot />
+</template>
---
+++ examples/nuxt/pages/mux-audio.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import "@mux/mux-audio";
+</script>
+
+<style>
+mux-audio {
+  width: 100%;
+  margin: 1rem 0 2rem;
+}
+</style>
+
+<template>
+  <mux-audio
+    playback-id="vDpm5ygrRJgfIEPNIc02IJR4Trf3z00AiP"
+    metadata-video-id="episode-id-12345"
+    metadata-video-title="Podcast: Episode 3"
+    metadata-viewer-user-id="user-id-6789"
+    muted
+    controls
+  ></mux-audio>
+
+  <RouterLink to="/">Browse Elements</RouterLink>
+</template>
