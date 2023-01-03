e6defd9ac5869850589eb3087b077b2e3bd86048
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-01-03 16:51:19 -0500

    Attempting to mock server

--- packages/mux-uploader/test/uploader.test.js
+++ packages/mux-uploader/test/uploader.test.js
@@ -1,7 +1,17 @@
-import { fixture, assert, aTimeout } from '@open-wc/testing';
+import { fixture, assert, expect, oneEvent, aTimeout } from '@open-wc/testing';
 import '../src/index.ts';
+import sinon from 'sinon';
 
 describe('<mux-uploader>', () => {
+  let server;
+  beforeEach(function () {
+    server = sinon.fakeServer.create();
+  });
+
+  afterEach(function () {
+    server.restore();
+  });
+
   it('initiates as expected', async function () {
     const uploader = await fixture(`<mux-uploader
       endpoint="https://my-authenticated-url/storage?your-url-params"
@@ -15,5 +25,36 @@ describe('<mux-uploader>', () => {
       'endpoint matches'
     );
     assert.equal(uploader.getAttribute('status'), '', 'status matches');
+  });
+
+  it('completes a mock upload', async function () {
+    server.respondWith('PUT', 'https://mock-upload-endpoint.com', [
+      200,
+      { 'Content-Type': 'application/json' },
+      '{success: true}',
+    ]);
+
+    const uploader = await fixture(`<mux-uploader
+      endpoint="/mock-upload-endpoint"
+      status
+    ></mux-uploader>`);
+
+    const file = new File(['foo'], 'foo.mp4', {
+      type: 'video/mp4',
+    });
+
+    uploader.dispatchEvent(
+      new CustomEvent('file-ready', {
+        composed: true,
+        bubbles: true,
+        detail: file,
+      })
+    );
+
+    const { detail } = await oneEvent(uploader, 'uploadstart');
+    await oneEvent(uploader, 'attempt');
+    await aTimeout(1000);
+    server.respond();
+    expect(server.requests.length).to.eq(1);
   });
 });
