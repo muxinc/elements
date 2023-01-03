c363b808f9a2fab277d02e0da150db4dce95369a
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-01-03 14:10:31 -0500

    Add basic init test

--- packages/mux-uploader/test/uploader.test.js
+++ packages/mux-uploader/test/uploader.test.js
@@ -1,5 +1,20 @@
 import { fixture, assert, aTimeout } from '@open-wc/testing';
 /** @todo Need to identify and resolve why upchunk causes issues in our testing environment. (TD). */
-// import '../src/index.ts';
+import '../src/index.ts';
 
-describe('<mux-uploader>', () => {});
+describe('<mux-uploader>', () => {
+  it('initiates as expected', async function () {
+    const uploader = await fixture(`<mux-uploader
+      endpoint="https://my-authenticated-url/storage?your-url-params"
+      status
+    ></mux-uploader>`);
+
+    assert.equal(uploader.getAttribute('type'), 'bar', 'default progress type is reflected');
+    assert.equal(
+      uploader.getAttribute('endpoint'),
+      'https://my-authenticated-url/storage?your-url-params',
+      'endpoint matches'
+    );
+    assert.equal(uploader.getAttribute('status'), '', 'status matches');
+  });
+});
