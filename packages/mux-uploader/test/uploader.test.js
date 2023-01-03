b0448806b693e00132f1fa4f5874cfcdd7aa3cd3
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-01-03 14:15:54 -0500

    Remove outdated comment

--- packages/mux-uploader/test/uploader.test.js
+++ packages/mux-uploader/test/uploader.test.js
@@ -1,5 +1,4 @@
 import { fixture, assert, aTimeout } from '@open-wc/testing';
-/** @todo Need to identify and resolve why upchunk causes issues in our testing environment. (TD). */
 import '../src/index.ts';
 
 describe('<mux-uploader>', () => {
