f81f6c678f9b98874aea907f371a02cca72f2275
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-01-05 12:46:23 -0500

    test(mux-uploader): add basic init test (#536)
    * Add basic init test
    
    * Remove outdated comment
    
    * Add sinon
    
    * Increase timeout
    
    * Attempting to mock server
    
    * mock server without sinon
    
    * Add coverage for mux-uploader-drop
    
    * Rename middleware
    
    * Ensure no endpoint fails
    
    * Add notes

--- packages/mux-uploader/test/uploader.test.js
+++ packages/mux-uploader/test/uploader.test.js
@@ -1,5 +1,71 @@
-import { fixture, assert, aTimeout } from '@open-wc/testing';
-/** @todo Need to identify and resolve why upchunk causes issues in our testing environment. (TD). */
-// import '../src/index.ts';
+import { fixture, assert, oneEvent } from '@open-wc/testing';
+import '../src/index.ts';
 
-describe('<mux-uploader>', () => {});
+describe('<mux-uploader>', () => {
+  let file = new File(['foo'], 'foo.mp4', {
+    type: 'video/mp4',
+  });
+
+  it('initiates as expected', async function () {
+    const uploader = await fixture(`<mux-uploader
+      endpoint="https://my-authenticated-url/storage?your-url-params"
+      status
+    ></mux-uploader>`);
+
+    assert.equal(uploader.getAttribute('type'), 'bar', 'default progress type is reflected');
+    assert.equal(
+      uploader.getAttribute('endpoint'),
+      'https://my-authenticated-url/storage?your-url-params',
+      'endpoint matches'
+    );
+    assert.equal(uploader.getAttribute('status'), '', 'status matches');
+  });
+
+  it('does not init without endpoint', async function () {
+    const uploader = await fixture(`<mux-uploader
+      status
+    ></mux-uploader>`);
+
+    setTimeout(() => {
+      uploader.dispatchEvent(
+        new CustomEvent('file-ready', {
+          composed: true,
+          bubbles: true,
+          detail: file,
+        })
+      );
+    });
+
+    const { detail } = await oneEvent(uploader, 'uploaderror');
+
+    assert.equal(detail.message, 'No url or endpoint specified -- cannot handleUpload', 'error message matches');
+    assert.exists(uploader.getAttribute('upload-error'), 'upload error is true');
+  });
+
+  it('completes a mock upload', async function () {
+    // Endpoint is a mock endpoint that returns a 200 response
+    // it is defined in the web-test-runner.config.mjs file as a middleware
+    const uploader = await fixture(`<mux-uploader
+      endpoint="/mock-upload-endpoint"
+      status
+    ></mux-uploader>`);
+
+    setTimeout(() => {
+      uploader.dispatchEvent(
+        new CustomEvent('file-ready', {
+          composed: true,
+          bubbles: true,
+          detail: file,
+        })
+      );
+    });
+
+    const { detail } = await oneEvent(uploader, 'uploadstart');
+    assert.equal(detail.chunkSize, 30720, 'chunk size matches');
+    assert.equal(detail.file, file, 'file matches');
+    assert.exists(uploader.getAttribute('upload-in-progress'), 'upload in progress is true');
+
+    await oneEvent(uploader, 'success');
+    assert.equal(uploader.statusMessage.innerHTML, 'Upload complete!', 'status message matches');
+  });
+});
--- packages/mux-uploader/test/web-test-runner.config.mjs
+++ packages/mux-uploader/test/web-test-runner.config.mjs
@@ -2,6 +2,24 @@ import { esbuildPlugin } from '@web/dev-server-esbuild';
 import { importMapsPlugin } from '@web/dev-server-import-maps';
 
 export default {
+  testFramework: {
+    config: {
+      ui: 'bdd',
+      timeout: '5000',
+    },
+  },
+  middleware: [
+    function respondWithUploadSuccess(context, next) {
+      // This is a mock endpoint that returns a 200 response
+      // It is used in the uploader.test.js file to test the upload process
+      if (context.url === '/mock-upload-endpoint') {
+        console.log(`Received ${context.headers['content-type']} request for ${context.url}`);
+        context.body = 'foo';
+        context.status = 200;
+      }
+      return next();
+    },
+  ],
   nodeResolve: true,
   plugins: [
     importMapsPlugin({
