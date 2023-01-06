c81ae19138c20b204f39a430e5c79a693e423525
Dave Kiss <dkiss@mux.com>
2023-01-06 08:33:29 -0500

    Rexport type for react package

--- packages/mux-uploader/src/index.ts
+++ packages/mux-uploader/src/index.ts
@@ -1,7 +1,8 @@
 export * as constants from './constants';
-import MuxUploader from './mux-uploader';
+import MuxUploader, { MuxUploaderElementEventMap } from './mux-uploader';
 import MuxUploaderProgressIndicator from './progress-indicator';
 import MuxUploaderDrop from './mux-uploader-drop';
 
 export { MuxUploaderDrop, MuxUploaderProgressIndicator };
+export type { MuxUploaderElementEventMap };
 export default MuxUploader;
