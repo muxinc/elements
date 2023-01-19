ca4e6e819344ed5e5d6cb2a42ad6f973fb111bed
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-01-19 07:41:39 -0500

    Revert "Shuffling around types"
    This reverts commit 0ea60c7742fced60f4d7adc4ecb80184bc627150.

--- packages/mux-uploader/src/index.ts
+++ packages/mux-uploader/src/index.ts
@@ -1,6 +1,5 @@
 export * as constants from './constants';
-import { MuxUploaderElementEventMap } from './types';
-import MuxUploaderElement from './mux-uploader';
+import MuxUploaderElement, { MuxUploaderElementEventMap } from './mux-uploader';
 import MuxUploaderProgressElement from './mux-uploader-progress';
 import MuxUploaderDropElement from './mux-uploader-drop';
 import MuxUploaderFileSelectElement from './mux-uploader-file-select';
