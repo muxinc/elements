b2bacbcf2a973077f9758f35b978823d0cdd449e
Christian Pillsbury <cpillsbury@mux.com>
2023-01-28 12:06:19 -0800

    feat(mux-viewer-count): Clean up scripts. Start refactor of element for typescript and different edge cases.

--- packages/mux-viewer-counts/src/index.ts
+++ packages/mux-viewer-counts/src/index.ts
@@ -1,60 +1,135 @@
 import { globalThis } from 'shared-polyfills';
 
-class MuxViewerCount extends globalThis.HTMLElement {
+const Attributes = {
+  TOKEN: 'token',
+  POLL_INTERVAL: 'pollinterval',
+};
+
+const DEFAULT_POLL_INTERVAL = 20;
+
+const AttributeValues = Object.freeze(Object.values(Attributes));
+
+/** @TODO Add code comments (CJP) */
+export const subscribeViewerCount = (
+  token: string,
+  pollInterval: number,
+  callback: (views: number) => void,
+  errorCb: (errorMsg: string) => void
+) => {
+  const url = `https://stats.mux.com/counts?token=${token}`;
+  const controller = new AbortController();
+  const signal = controller.signal;
+  let timeoutId: number | undefined;
+  let aborted = false;
+  const fetchViewerCountPoll: () => Promise<any> = async () => {
+    if (aborted) return Promise.resolve();
+    return fetch(url, { signal })
+      .then((resp) => resp.json())
+      .then((respObj) => {
+        const views = respObj?.data?.[0]?.views;
+        if (!!respObj?.error || views == null) {
+          errorCb(respObj?.error ?? 'no data in response');
+          return;
+        }
+        callback(views);
+        return views;
+      })
+      .catch(errorCb)
+      .then(() => {
+        return new Promise((resolve) => {
+          timeoutId = setTimeout(async () => {
+            resolve(undefined);
+          }, pollInterval * 1000);
+        });
+      })
+      .then(fetchViewerCountPoll);
+  };
+
+  fetchViewerCountPoll();
+
+  return () => {
+    aborted = true;
+    controller.abort();
+    if (typeof timeoutId === 'number') {
+      clearTimeout(timeoutId);
+      timeoutId = undefined;
+    }
+  };
+};
+
+class MuxViewerCountElement extends globalThis.HTMLElement {
+  static get observedAttributes() {
+    return AttributeValues;
+  }
+
+  #unsubscribeViewerCount: (() => void) | undefined;
+
   constructor() {
     super();
   }
 
   get token() {
-    return this.getAttribute('token');
+    return this.getAttribute(Attributes.TOKEN) ?? '';
   }
 
-  get interval() {
-    return this.getAttribute('interval')*1000;
+  set token(value: string) {
+    if (this.token === value) return;
+    this.setAttribute(Attributes.TOKEN, value);
   }
 
-  disconnectedCallback() {
-    clearInterval(this.myInterval);
+  get pollInterval() {
+    return this.hasAttribute(Attributes.POLL_INTERVAL)
+      ? +(this.getAttribute(Attributes.POLL_INTERVAL) as string)
+      : DEFAULT_POLL_INTERVAL;
   }
 
-  getViewCount(myToken) {
-    
-    const url = `https://stats.mux.com/counts?token=${myToken}`
-    let success = 1;
-
-    try {
-      var xmlHttp = new XMLHttpRequest();
-      xmlHttp.open("GET", url, false);
-      xmlHttp.send(null);
-    } catch (exception) {
-      success = 0;
-      console.warn("Error retrieving media-viewer-count data: "+exception)
-    }
-    if (success) {
-      let responseObj = JSON.parse(xmlHttp.responseText);
-      const hasError = Object.prototype.hasOwnProperty.call(responseObj,'error');
-      const hasData = Object.prototype.hasOwnProperty.call(responseObj,'data');
-      if (hasError) {
-        console.warn("Error retrieving media-viewer-count data: "+responseObj.error);
-        return false;
-      } else if (hasData) {
-        let data = responseObj.data[0];
-        this.textContent = data.views;
-      } else {
-        console.warn("Error retrieving media-viewer-count data: no data in response");
-        return false;
-      }
-    }
+  set pollInterval(value: number) {
+    if (this.pollInterval === value) return;
+    this.setAttribute(Attributes.POLL_INTERVAL, `${value}`);
   }
 
   connectedCallback() {
-    this.getViewCount(this.token);
-    this.myInterval = setInterval(this.getViewCount.bind(this),this.interval,this.token);
+    this.#setupViewerCountPolling();
+  }
+
+  disconnectedCallback() {
+    this.#teardownViewerCountPolling();
+  }
+
+  attributeChangedCallback(attrName: string, oldValue: string | null, newValue: string | null) {
+    /** @TODO Support dynamic (re)setting of attributes (CJP) */
+  }
+
+  #setupViewerCountPolling() {
+    if (this.token && this.pollInterval && !this.#unsubscribeViewerCount) {
+      this.#unsubscribeViewerCount = subscribeViewerCount(
+        this.token,
+        this.pollInterval,
+        // Success callback
+        (views) => {
+          /** @TODO Add event for viewschange (CJP) */
+          /** @TODO Add views getter (CJP) */
+          /** @TODO Add "beefier" template (CJP) */
+          this.textContent = `${views}`;
+        },
+        // Error callback
+        (errorMsg) => {
+          /** @TODO Add event for error (CJP) */
+          /** @TODO Consider adding retry count/logic (CJP) */
+          this.#teardownViewerCountPolling();
+        }
+      );
+    }
+  }
+
+  #teardownViewerCountPolling() {
+    this.#unsubscribeViewerCount?.();
+    this.#unsubscribeViewerCount = undefined;
   }
 }
 
 if (!globalThis.customElements.get('media-viewer-count')) {
-  globalThis.customElements.define('media-viewer-count', MediaViewerCount);
+  globalThis.customElements.define('media-viewer-count', MuxViewerCountElement);
 }
 
-export default MediaViewerCount;
+export default MuxViewerCountElement;
