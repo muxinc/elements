80a6899b411c342419d6f11122a56a620f0623df
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-01-31 16:29:28 -0500

    Add to exports

--- packages/mux-uploader/src/index.ts
+++ packages/mux-uploader/src/index.ts
@@ -4,7 +4,14 @@ import MuxUploaderProgressElement from './mux-uploader-progress';
 import MuxUploaderDropElement from './mux-uploader-drop';
 import MuxUploaderFileSelectElement from './mux-uploader-file-select';
 import MuxUploaderSrTextElement from './mux-uploader-sr-text';
+import MuxUploaderStatusElement from './mux-uploader-status';
 
-export { MuxUploaderDropElement, MuxUploaderProgressElement, MuxUploaderFileSelectElement, MuxUploaderSrTextElement };
+export {
+  MuxUploaderDropElement,
+  MuxUploaderProgressElement,
+  MuxUploaderFileSelectElement,
+  MuxUploaderSrTextElement,
+  MuxUploaderStatusElement,
+};
 export type { MuxUploaderElementEventMap };
 export default MuxUploaderElement;
