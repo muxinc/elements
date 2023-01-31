f2e157b7755869b4b7ca194e2b8b9f64368a3b28
Christian Pillsbury <cpillsbury@mux.com>
2023-01-31 10:58:36 -0800

    test(mux-viewer-count): Fix stub test case.

--- packages/mux-viewer-count/test/index.test.js
+++ packages/mux-viewer-count/test/index.test.js
@@ -3,6 +3,6 @@ import MuxViewerCount from '../src/index.ts';
 
 describe('<mux-viewer-count>', () => {
   it('needs real tests', async function () {
-    assert.true(true);
+    assert.isTrue(true);
   });
 });
