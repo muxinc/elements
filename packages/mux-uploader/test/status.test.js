db1b4c9122f087dc1e255268478a845c1a414865
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-01-31 17:14:28 -0500

    Add tests for error and reset

--- packages/mux-uploader/test/status.test.js
+++ packages/mux-uploader/test/status.test.js
@@ -4,11 +4,11 @@ import { fixture, assert, oneEvent, aTimeout } from '@open-wc/testing';
 import '../src/index.ts';
 
 describe('<mux-uploader-status>', () => {
-  it('updates with success message on success', async function () {
-    let file = new File(['foo'], 'foo.mp4', {
-      type: 'video/mp4',
-    });
+  let file = new File(['foo'], 'foo.mp4', {
+    type: 'video/mp4',
+  });
 
+  it('updates with success message on success', async function () {
     server.respondWith('PUT', 'https://mock-upload-endpoint.com', [
       200,
       { 'Content-Type': 'application/json' },
@@ -35,5 +35,45 @@ describe('<mux-uploader-status>', () => {
     server.respond();
     await oneEvent(uploader, 'success');
     assert.equal(status.statusMessage.innerHTML, 'Upload complete!', 'status message matches');
+  });
+
+  it('show an error when not provided an endpoint', async function () {
+    const uploader = await fixture(`<mux-uploader></mux-uploader>`);
+    const status = uploader.shadowRoot.querySelector('mux-uploader-status');
+
+    setTimeout(() => {
+      uploader.dispatchEvent(
+        new CustomEvent('file-ready', {
+          composed: true,
+          bubbles: true,
+          detail: file,
+        })
+      );
+    });
+
+    const { detail } = await oneEvent(uploader, 'uploaderror');
+    assert.equal(status.statusMessage.innerHTML, detail.message, 'status message matches');
+  });
+
+  it('clears an error on retry click', async function () {
+    const uploader = await fixture(`<mux-uploader></mux-uploader>`);
+    const status = uploader.shadowRoot.querySelector('mux-uploader-status');
+
+    setTimeout(() => {
+      uploader.dispatchEvent(
+        new CustomEvent('file-ready', {
+          composed: true,
+          bubbles: true,
+          detail: file,
+        })
+      );
+    });
+
+    await oneEvent(uploader, 'uploaderror');
+
+    const listener = oneEvent(uploader, 'reset');
+    uploader.retryButton.click();
+    const e = await listener;
+    assert.equal(status.statusMessage.innerHTML, '', 'status message cleared');
   });
 });
