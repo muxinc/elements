3b40733b3dfbc8e3e2e839a971a556fa476b1167
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-02-08 11:21:09 -0500

    Test keyup

--- packages/mux-uploader/test/retry.test.js
+++ packages/mux-uploader/test/retry.test.js
@@ -18,4 +18,22 @@ describe('<mux-uploader-retry>', () => {
     const e = await listener;
     assert.exists(e);
   });
+
+  it('fires a reset event on Enter keypress', async function () {
+    const uploader = await fixture(`<mux-uploader
+    endpoint="https://mock-upload-endpoint.com"
+  ></mux-uploader>`);
+
+    const retry = uploader.shadowRoot.querySelector('mux-uploader-retry');
+
+    const listener = oneEvent(uploader, 'reset');
+    retry.retryButton.dispatchEvent(
+      new KeyboardEvent('keyup', {
+        key: 'Enter',
+      })
+    );
+
+    const e = await listener;
+    assert.exists(e);
+  });
 });
