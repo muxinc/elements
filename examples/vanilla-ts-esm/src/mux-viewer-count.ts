99af547ecb6b54457404ae9da3b87e5742394858
Jared Smith <jsmith@mux.com>
2023-02-17 18:17:22 -0500

    A few more renames
    Signed-off-by: Jared Smith <jsmith@mux.com>

--- examples/vanilla-ts-esm/src/mux-viewer-count.ts
+++
@@ -1,1 +0,0 @@
-export * as MuxViewerCountElement from "@mux/mux-viewer-count";
--- packages/mux-active-viewer-count/test/index.test.js
+++ packages/mux-active-viewer-count/test/index.test.js
@@ -1,5 +1,5 @@
 import { fixture, assert, aTimeout } from '@open-wc/testing';
-import MuxViewerCount from '../src/index.ts';
+import MuxActiveViewerCount from '../src/index.ts';
 
 describe('<mux-active-viewer-count>', () => {
   it('needs real tests', async function () {
