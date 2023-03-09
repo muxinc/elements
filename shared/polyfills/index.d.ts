e21f355dc75a3c393ca8a02533966827cf894978
Gary Katsevman <gary@mux.com>
2023-03-09 16:14:56 -0500

    remove amient module from shared-polyfills

--- shared/polyfills/index.d.ts
+++ shared/polyfills/index.d.ts
@@ -1,5 +1,3 @@
-declare module 'shared-polyfills';
-
 declare const GlobalThis: Window & typeof globalThis;
 declare const Document: Document;
 export { GlobalThis as globalThis, Document as document };
