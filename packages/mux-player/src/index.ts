5c366b47409e89af80d465e87c3c04c50ee90d31
Wesley Luyten <me@wesleyluyten.com>
2023-03-13 19:47:47 -0500

    fix: make getMetadataFromAttrs a private function

--- packages/mux-player/src/index.ts
+++ packages/mux-player/src/index.ts
@@ -135,6 +135,19 @@ function getThemeTemplate(el: MuxPlayerElement) {
   }
 }
 
+function getMetadataFromAttrs(el: MuxPlayerElement) {
+  return el
+    .getAttributeNames()
+    .filter((attrName) => attrName.startsWith('metadata-'))
+    .reduce((currAttrs, attrName) => {
+      const value = el.getAttribute(attrName);
+      if (value !== null) {
+        currAttrs[attrName.replace(/^metadata-/, '').replace(/-/g, '_')] = value;
+      }
+      return currAttrs;
+    }, {} as { [key: string]: string });
+}
+
 const MuxVideoAttributeNames = Object.values(MuxVideoAttributes);
 const VideoAttributeNames = Object.values(VideoAttributes);
 const PlayerAttributeNames = Object.values(PlayerAttributes);
@@ -279,22 +292,10 @@ class MuxPlayerElement extends VideoApiElement implements MuxPlayerElement {
     return this.mediaTheme?.shadowRoot?.querySelector('media-controller');
   }
 
-  get metadataFromAttrs() {
-    return this.getAttributeNames()
-      .filter((attrName) => attrName.startsWith('metadata-'))
-      .reduce((currAttrs, attrName) => {
-        const value = this.getAttribute(attrName);
-        if (value !== null) {
-          currAttrs[attrName.replace(/^metadata-/, '').replace(/-/g, '_')] = value;
-        }
-        return currAttrs;
-      }, {} as { [key: string]: string });
-  }
-
   connectedCallback() {
     const muxVideo = this.shadowRoot?.querySelector('mux-video') as MuxVideoElement;
     if (muxVideo) {
-      muxVideo.metadata = this.metadataFromAttrs;
+      muxVideo.metadata = getMetadataFromAttrs(this);
     }
   }
 
@@ -1158,7 +1159,7 @@ class MuxPlayerElement extends VideoApiElement implements MuxPlayerElement {
       logger.error('underlying media element missing when trying to set metadata. metadata will not be set.');
       return;
     }
-    this.media.metadata = { ...this.metadataFromAttrs, ...val };
+    this.media.metadata = { ...getMetadataFromAttrs(this), ...val };
   }
 
   async addCuePoints<T = any>(cuePoints: { time: number; value: T }[]) {
