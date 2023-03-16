2bcc7ef8b4191cdede38cae0e65f836f0988c1ba
Christian Pillsbury <cpillsbury@mux.com>
2023-03-16 08:09:02 -0700

    chore(mux-player-react): More cleanup/polish for cuepoints example.

--- examples/nextjs-with-typescript/pages/MuxPlayerCuePointsMeditate.tsx
+++ examples/nextjs-with-typescript/pages/MuxPlayerCuePointsMeditate.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Head from 'next/head';
+import Image from "next/image";
 import { useReducer, useRef, useState, useEffect } from "react";
 import type { CSSProperties } from "react";
-import MuxPlayer, { MuxPlayerProps } from "@mux/mux-player-react";
+import MuxPlayer from "@mux/mux-player-react";
 import type MuxPlayerElement from "@mux/mux-player";
 
 const formatTime = (seconds: number | undefined) => {
@@ -26,7 +27,6 @@ type CuePointValue = {
 
 type AbbreviableCuePointValue = CuePointValue & {
   type: 'abbreviable';
-  apply?: boolean;
   duration?: number;
 };
 
@@ -41,7 +41,7 @@ const initialCuePoints: CuePoint[] = [
     }
   },
   {
-    time: 60 * 6 + 7,
+    time: 60 * 6 + 2,
     value: {
       description: "Preparing to begin meditation practice"
     }
@@ -63,8 +63,7 @@ const initialCuePoints: CuePoint[] = [
     value: {
       type: "abbreviable",
       description: "Silent self practice",
-      duration: undefined,
-      apply: false,
+      duration: 10,
     }
   },
   {
@@ -83,78 +82,41 @@ const initialCuePoints: CuePoint[] = [
 ];
 
 const CuePointRenderer = ({
-  time,
-  value,
+  cuePoint,
   active = false,
-  maxDuration = Number.POSITIVE_INFINITY,
-  onSelected = noop,
-  onSkipChange = noop,
-  onDurationChange = noop,
-  onApplyChange = noop,
+  disableUpdates = false,
+  onCuePointSelected = noop,
+  onCuePointChange = noop,
 }: {
-  time: number;
-  value: CuePoint['value'];
+  cuePoint: CuePoint;
   active?: boolean;
-  maxDuration?: number;
-  onSelected?: ((time: number) => void);
-  onSkipChange?: ((skip: boolean) => void);
-  onDurationChange?: ((duration: number) => void);
-  onApplyChange?: ((apply: boolean) => void);
+  disableUpdates?: boolean;
+  onCuePointSelected?: ((cuePoint: CuePoint) => void);
+  onCuePointChange?: ((cuePoint: CuePoint) => void);
 }) => {
+  const { time, value } = cuePoint;
   const { description, skip = false } = value;
   const skipInputId = `cuepoint-skip-${time}`;
   const activeStyle: CSSProperties = active ? { backgroundColor: 'lightgreen' } : {};
   return (<>
     <button
       style={activeStyle}
-      onClick={() => onSelected(time)}
+      onClick={() => onCuePointSelected(cuePoint)}
       disabled={skip}
     >
       {formatTime(time)}
     </button>
-    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      <div>{description}</div>
-      {value.type === "abbreviable" && (
-      <div style={{ display: 'flex', padding: '0px 5px' }}>
-        <div>
-          <input
-            style={{ width: '80%' }}
-            type="number"
-            min={0}
-            max={maxDuration / 60}
-            step={1}
-            placeholder="Entire duration"
-            onInput={({ target }) => {
-              const inputValue = (target as HTMLInputElement).value;
-              console.log('inputValue', inputValue);
-              const newDuration = inputValue === ''
-                ? undefined
-                : +inputValue * 60;
-              onDurationChange(newDuration);
-            }}
-          />
-          <div>
-            <label>Apply</label>
-            <input
-              type={"checkbox"}
-              checked={(value as AbbreviableCuePointValue).apply}
-              disabled={isNil((value as AbbreviableCuePointValue).duration)}
-              onChange={({ target }) => {
-                onApplyChange(target.checked);
-              }}
-            />
-            Duration (minutes, max 10)
-          </div>
-        </div>
-      </div>
-      )}
-    </div>
+    <div>{description}</div>
     <div>
       <input
         id={skipInputId}
         type={"checkbox"}
         defaultChecked={skip}
-        onChange={({ target }) => onSkipChange(target.checked)}
+        disabled={disableUpdates}
+        onChange={({ target }) => {
+          const skip = target.checked;
+          onCuePointChange({ time, value: { ...value, skip } });
+        }}
       />
       <label htmlFor={skipInputId}>Skip</label>
     </div>
@@ -164,11 +126,13 @@ const CuePointRenderer = ({
 const CuePointsList = ({
   cuePoints = [],
   activeCuePoint,
+  disableUpdates = false,
   onCuePointSelected = noop,
   onCuePointChange = noop,
 }: {
   cuePoints: CuePoint[];
   activeCuePoint?: CuePoint;
+  disableUpdates?: boolean;
   onCuePointSelected?: ((cuePoint: CuePoint) => void);
   onCuePointChange?: (cuePoint: CuePoint) => void;
 }) => {
@@ -182,27 +146,16 @@ const CuePointsList = ({
       }}
     >
       {cuePoints.map((cuePoint, i) => {
-        return (<CuePointRenderer
-              key={cuePoint.time}
-              time={cuePoint.time}
-              value={cuePoint.value}
-              active={activeCuePoint?.time === cuePoint.time}
-              maxDuration={((cuePoints[i + 1]?.time ?? Number.POSITIVE_INFINITY) - cuePoint.time)}
-              onSelected={(_time: number) => {
-                onCuePointSelected(cuePoint);
-              }}
-              onSkipChange={(skip) => {
-                onCuePointChange({ time: cuePoint.time, value: { ...cuePoint.value, skip } });
-              }}
-              onApplyChange={(apply) => {
-                console.log('apply change!', apply);
-                onCuePointChange({ time: cuePoint.time, value: { ...cuePoint.value, apply } });
-              }}
-              onDurationChange={(duration) => {
-                onCuePointChange({ time: cuePoint.time, value: { ...cuePoint.value, duration, apply: !isNil(duration) } });
-              }}
-            />
-          );
+        return (
+          <CuePointRenderer
+            key={cuePoint.time}
+            cuePoint={cuePoint}
+            disableUpdates={disableUpdates}
+            active={activeCuePoint?.time === cuePoint.time}
+            onCuePointChange={onCuePointChange}
+            onCuePointSelected={onCuePointSelected}
+          />
+        );
       })}
     </div>
   );
@@ -212,7 +165,7 @@ const addCuePointsToPlayer = (cuePoints: CuePoint[], playerEl: MuxPlayerElement)
   const actualCuePoints = cuePoints.reduce((cuePoints, cuePoint) => {
     if (cuePoint.value.type === 'abbreviable') {
       const value: AbbreviableCuePointValue = cuePoint.value as AbbreviableCuePointValue;
-      if (value.apply && !isNil(value.duration)) {
+      if (!isNil(value.duration)) {
         const duration: number = value.duration;
         const insertedSkipCuePoint = {
           time: cuePoint.time + duration,
@@ -238,7 +191,6 @@ type ActionType = 'UPDATE';
 const reducer = (cuePoints: CuePoint[], action: { type: ActionType, value: CuePoint }) => {
   if (action.type === 'UPDATE') {
     const cuePointIdx = cuePoints.findIndex(cuePoint => cuePoint.time === action.value.time);
-    console.log('next CuePoint value', action.value);
     return [
       ...cuePoints.slice(0, cuePointIdx),
       action.value,
@@ -248,32 +200,94 @@ const reducer = (cuePoints: CuePoint[], action: { type: ActionType, value: CuePo
   return cuePoints;
 };
 
+const UCLAHeader = () => {
+  return (
+    <header style={{ display: "flex", justifyContent: 'space-between', alignItems: 'center', padding: '0em .9em' }}>
+      <Link href="https://www.uclahealth.org/">
+        <a>
+          <Image width="200" height="50" src="/images/ucla-health-logo.svg" alt="UCLA Health logo" />
+        </a>
+      </Link>
+      <Link href="https://www.uclahealth.org/programs/marc">
+        <a>
+          <h2>Mindful Awareness Research Center (MARC)</h2>
+        </a>
+      </Link>
+    </header>
+  );
+};
+
+const DurationList = ({
+  values = [],
+  selectedValue,
+  disableUpdates = false,
+  onSelected = noop
+}: {
+  values: number[];
+  selectedValue?: number,
+  disableUpdates?: boolean
+  onSelected?: (duration: number) => void;
+}) => {
+  return (
+    <div style={{ display: "flex", justifyContent: 'space-evenly'}}>
+      {values.map(duration => {
+        const selectedStyle: CSSProperties = duration === selectedValue ? { backgroundColor: 'lightgreen' } : {};
+        return (
+          <button
+            disabled={disableUpdates}
+            style={selectedStyle}
+            key={duration}
+            onClick={() => {
+              onSelected(duration);
+            }}
+          >
+            {Math.round(duration / 60)} min
+          </button>
+        );
+      })}
+    </div>
+  );
+}
+
 function MuxPlayerPage() {
+  const [cuePoints, dispatch] = useReducer(reducer, initialCuePoints);
+  const [abbreviableCuePointIndex, setAbbreviableCuePoint] = useState<number>(undefined);
   const playerElRef = useRef(null);
   const [activeCuePoint, setActiveCuePoint] = useState<CuePoint>(undefined);
-  const [cuePoints, dispatch] = useReducer(reducer, initialCuePoints);
   const [startTime, setStartTime] = useState<number>(undefined);
+  const [cuePointsAdded, setCuePointsAdded] = useState(false);
 
   useEffect(() => {
     const nextStartTime = cuePoints.find(cuePoint => !(cuePoint.value.skip))?.time;
     if (nextStartTime === startTime) return;
-    setStartTime(nextStartTime - 1);
+    setStartTime(nextStartTime);
   }, [cuePoints, startTime]);
+
+  useEffect(() => {
+    const idx = cuePoints.findIndex(({ value: { type }}) => type === 'abbreviable');
+    if (idx === abbreviableCuePointIndex) return;
+    setAbbreviableCuePoint(idx);
+  }, [cuePoints, abbreviableCuePointIndex]);
 
   useEffect(() => {
     setCurrentTimeOnPlayer(startTime ?? 0, playerElRef.current);
   }, [startTime]);
-
-  useEffect(() => {
-    // console.log(...cuePoints);
-    console.log(cuePoints.find(cuePoint => cuePoint.value.type === 'abbreviable'));
-  }, [cuePoints]);
 
   return (
     <>
       <Head>
         <title>&lt;MuxPlayer/&gt; (CuePoints) Demo</title>
       </Head>
+      <UCLAHeader/>
+      <section style={{ padding: '0em .9em'}}>
+        <h1>About this App</h1>
+        <h4>
+          MARC provides fantastic, free guided meditations, but wouldn't it be nice if you could just dive right into the meditation practice?
+          Wouldn't it be even better if you could adjust how long the silent self-practice was based on your level of comfort or available time?
+          That's exactly what this application lets you do!
+        </h4>
+      </section>
+      <section>
       <MuxPlayer
         ref={playerElRef}
         style={{ background: 'black' }}
@@ -286,31 +300,54 @@ function MuxPlayerPage() {
         onPlay={({ target }) => {
           const muxPlayerEl = target as MuxPlayerElement;
           addCuePointsToPlayer(cuePoints, muxPlayerEl);
+          setCuePointsAdded(true);
         }}
         onCuePointChange={({ target }) => {
           const muxPlayerEl = target as MuxPlayerElement;
           const activeCuePoint = muxPlayerEl.activeCuePoint;
           setActiveCuePoint(activeCuePoint);
+          // If we're playing through and the activeCuePoint is one that
+          // should be skipped, skip it.
           if (!muxPlayerEl.paused && activeCuePoint.value.skip) {
             const cuePoints = muxPlayerEl.cuePoints;
+            // Find the next CuePoint that shouldn't be skipped
             const nextCuePointToPlay = cuePoints.find(({ time, value: { skip } }) => time > activeCuePoint.time && !skip);
+            // Seek to the time of the next unskipped CuePoint (or to the end of the content if there are none)
             setCurrentTimeOnPlayer(
               nextCuePointToPlay
-                ? nextCuePointToPlay.time + 1
+                ? nextCuePointToPlay.time
                 : muxPlayerEl.duration,
               muxPlayerEl
             );
           }
         }}
-        onCuePointsChange={({ target }) => {
-          const muxPlayerEl = target as MuxPlayerElement;
-          const activeCuePoint = muxPlayerEl.activeCuePoint;
-          setActiveCuePoint(activeCuePoint);
+        onEnded={() => {
+          setActiveCuePoint(undefined);
         }}
       />
+      <div>
+        <DurationList
+          values={[1 * 60, 2 * 60, 5 * 60, 10 * 60]}
+          selectedValue={(cuePoints[abbreviableCuePointIndex]?.value as AbbreviableCuePointValue)?.duration}
+          disableUpdates={cuePointsAdded}
+          onSelected={(duration) => {
+            if (abbreviableCuePointIndex >= 0) {
+              const cuePoint = {
+                time: cuePoints[abbreviableCuePointIndex].time,
+                value: {
+                  ...cuePoints[abbreviableCuePointIndex].value,
+                  duration
+                }
+              }
+              dispatch({ type: 'UPDATE', value: cuePoint });
+            }
+          }}
+        />
+      </div>
       <CuePointsList
         cuePoints={cuePoints}
         activeCuePoint={activeCuePoint}
+        disableUpdates={cuePointsAdded}
         onCuePointSelected={({ time }) => {
           setCurrentTimeOnPlayer(time, playerElRef.current)
         }}
@@ -318,10 +355,30 @@ function MuxPlayerPage() {
           dispatch({ type: 'UPDATE', value: cuePoint });
         }}
       />
-      <br />
+      </section>
+      <footer>
+        <p>
+          <span>This content is part of UCLA MARC's "Drop in Meditations" series, graciously provided under a Creative Commons License. You may find a list of all Drop in meditations </span>
+          <Link href="https://www.uclahealth.org/programs/marc/free-guided-meditations/drop-meditations-hammer-podcast">
+            <a>here</a>
+          </Link>
+          <span>.</span>
+        </p>
+
+        <p>“Drop-in Meditations” created by Diana Winston and others (see above) for the UCLA Mindful Awareness Research Center (MARC), ©2011- 2021 The Regents of the University of California (The UC Regents).</p>
+        <p>Drop-in Meditations are licensed under a Creative Commons Attribution, NonCommercial, NoDerivatives 4.0 International License.</p>
+      </footer>
       <Link href="/"><a>Browse Elements</a></Link>
     </>
   );
 }
 
 export default MuxPlayerPage;
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      hideDefaultHeader: true
+    }
+  };
+}
