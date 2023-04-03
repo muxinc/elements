981ef65dd0dd950d905305332bb484a01e360104
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-04-03 08:38:44 -0400

    Conditionally render els if they don't exist in DOM

--- packages/mux-uploader/src/layouts/block.ts
+++ packages/mux-uploader/src/layouts/block.ts
@@ -3,26 +3,47 @@ import { document } from '../polyfills';
 import '../mux-uploader-file-select';
 import { fileSelectFragment } from '../mux-uploader-file-select';
 
+function shouldRenderElement(selector: string): boolean {
+  return !document.querySelector(selector);
+}
+
+function renderElement(selector: string, template: string): string {
+  return shouldRenderElement(selector) ? template : '';
+}
+
+function shouldRenderWrapper(nodrop: boolean, selector: string): string {
+  return !nodrop && !document.querySelector(selector) ? 'mux-uploader-drop overlay' : 'div';
+}
+
 export default function blockLayout(nodrop: boolean) {
-  const wrapper = nodrop ? 'div' : 'mux-uploader-drop overlay';
+  const wrapper = shouldRenderWrapper(nodrop, 'mux-uploader-drop');
 
-  return document.createRange().createContextualFragment(`
-    <style>
-      /* Add your styles here */
-    </style>
-
-    <${wrapper}>
-      <mux-uploader-status></mux-uploader-status>
-      <mux-uploader-retry></mux-uploader-retry>
-
-      <mux-uploader-file-select>
+  const renderStatus = renderElement('mux-uploader-status', '<mux-uploader-status></mux-uploader-status>');
+  const renderRetry = renderElement('mux-uploader-retry', '<mux-uploader-retry></mux-uploader-retry>');
+  const renderPercentageProgress = renderElement(
+    'mux-uploader-progress[type="percentage"]',
+    '<mux-uploader-progress type="percentage"></mux-uploader-progress>'
+  );
+  const renderFileSelect = renderElement(
+    'mux-uploader-file-select',
+    `<mux-uploader-file-select>
         <slot name="file-select">
           ${fileSelectFragment}
         </slot>
-      </mux-uploader-file-select>
+      </mux-uploader-file-select>`
+  );
+  const renderProgress = renderElement(
+    'mux-uploader-progress:not([type])',
+    '<mux-uploader-progress></mux-uploader-progress>'
+  );
 
-      <mux-uploader-progress type="percentage"></mux-uploader-progress>
-      <mux-uploader-progress></mux-uploader-progress>
+  return document.createRange().createContextualFragment(`
+    <${wrapper}>
+      ${renderStatus}
+      ${renderRetry}
+      ${renderFileSelect}
+      ${renderPercentageProgress}
+      ${renderProgress}
     </${wrapper}>
   `);
 }
