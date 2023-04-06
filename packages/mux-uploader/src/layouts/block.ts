59a09789fa4a0c03bfae448b44ace03638d83d06
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-04-06 10:27:10 -0400

    feat(mux-uploader): conditional render based on attributes (#645)
    This PR adds support for conditional rendering of `mux-uploader`
    components, including `nodrop`, `noprogress`, `nostatus`, and `noretry`.
    By passing the `MuxUploader` element to the `blockLayout` function,
    users can now choose to hide specific components based on their needs.
    
    Usage:
    
    ```html
    <mux-uploader nodrop nostatus></mux-uploader>
    ```
    
    In this example, `<mux-uploader>` will be rendered without the drop area
    and status components, while keeping the progress and retry components.

--- packages/mux-uploader/src/layouts/block.ts
+++ packages/mux-uploader/src/layouts/block.ts
@@ -2,18 +2,36 @@ import { document } from '../polyfills';
 
 import '../mux-uploader-file-select';
 import { fileSelectFragment } from '../mux-uploader-file-select';
+import MuxUploaderElement from '../mux-uploader';
 
-export default function blockLayout(nodrop: boolean) {
+interface BlockLayoutOptions {
+  nodrop?: boolean;
+  noprogress?: boolean;
+  nostatus?: boolean;
+  noretry?: boolean;
+}
+
+function conditionalRender(flag: boolean | undefined, component: string): string {
+  return flag ? '' : component;
+}
+
+export default function blockLayout(contextElement: MuxUploaderElement): DocumentFragment {
+  const { nodrop, noprogress, nostatus, noretry } = contextElement;
   const wrapper = nodrop ? 'div' : 'mux-uploader-drop overlay';
+  const progressElements = conditionalRender(
+    noprogress,
+    `
+      <mux-uploader-progress type="percentage"></mux-uploader-progress>
+      <mux-uploader-progress></mux-uploader-progress>
+    `
+  );
+  const statusElement = conditionalRender(nostatus, '<mux-uploader-status></mux-uploader-status>');
+  const retryElement = conditionalRender(noretry, '<mux-uploader-retry></mux-uploader-retry>');
 
   return document.createRange().createContextualFragment(`
-    <style>
-      /* Add your styles here */
-    </style>
-
     <${wrapper}>
-      <mux-uploader-status></mux-uploader-status>
-      <mux-uploader-retry></mux-uploader-retry>
+      ${statusElement}
+      ${retryElement}
 
       <mux-uploader-file-select>
         <slot name="file-select">
@@ -21,8 +39,7 @@ export default function blockLayout(nodrop: boolean) {
         </slot>
       </mux-uploader-file-select>
 
-      <mux-uploader-progress type="percentage"></mux-uploader-progress>
-      <mux-uploader-progress></mux-uploader-progress>
+      ${progressElements}
     </${wrapper}>
   `);
 }
