abc38f34f27b2b3c2d0bfbe741612586b296f0f4
Christian Pillsbury <cpillsbury@mux.com>
2023-04-10 14:54:03 -0700

    docs(mux-player, mux-player-react): update docs to presume old stream types. Add tests for translation cases and other.

--- packages/mux-player/test/player.test.js
+++ packages/mux-player/test/player.test.js
@@ -1132,6 +1132,69 @@ describe('<mux-player> seek to live behaviors', function () {
   });
 });
 
+// NOTE: When we fully deprecate "old" stream types ("ll-" prefix and "dvr"), we should update these tests accordingly.
+describe('Feature: stream types & related (including non-media-ui-extension types', async () => {
+  it('should set expected default values for stream type on-demand', async () => {
+    const muxPlayerEl = await fixture(`<mux-player
+      stream-type="on-demand"
+    ></mux-player>`);
+    assert.equal(muxPlayerEl.streamType, 'on-demand');
+    assert(Number.isNaN(muxPlayerEl.targetLiveWindow), 'targetLiveWindow should be NaN for on-demand');
+    assert.equal(muxPlayerEl.media.streamType, 'on-demand');
+  });
+
+  it('should set expected default values for stream type live', async () => {
+    const muxPlayerEl = await fixture(`<mux-player
+      stream-type="live"
+    ></mux-player>`);
+    assert.equal(muxPlayerEl.streamType, 'live');
+    assert(!muxPlayerEl.targetLiveWindow, '!targetLiveWindow for live');
+    assert.equal(muxPlayerEl.media.streamType, 'live');
+  });
+
+  it('should set expected default values for stream type ll-live', async () => {
+    const muxPlayerEl = await fixture(`<mux-player
+      stream-type="ll-live"
+    ></mux-player>`);
+    assert.equal(muxPlayerEl.streamType, 'll-live');
+    assert(!muxPlayerEl.targetLiveWindow, '!targetLiveWindow for ll-live');
+    // Apply the "translated" stream type to underlying `<mux-video>` instance.
+    assert.equal(muxPlayerEl.media.streamType, 'live');
+  });
+
+  it('should set expected default values for stream type live:dvr', async () => {
+    const muxPlayerEl = await fixture(`<mux-player
+      stream-type="live:dvr"
+    ></mux-player>`);
+    assert.equal(muxPlayerEl.streamType, 'live:dvr');
+    assert.equal(muxPlayerEl.targetLiveWindow, Number.POSITIVE_INFINITY);
+    // Apply the "translated" stream type to underlying `<mux-video>` instance.
+    assert.equal(muxPlayerEl.media.streamType, 'live');
+  });
+
+  it('should set expected default values for stream type ll-live:dvr', async () => {
+    const muxPlayerEl = await fixture(`<mux-player
+      stream-type="ll-live:dvr"
+    ></mux-player>`);
+    assert.equal(muxPlayerEl.streamType, 'll-live:dvr');
+    assert.equal(muxPlayerEl.targetLiveWindow, Number.POSITIVE_INFINITY);
+    // Apply the "translated" stream type to underlying `<mux-video>` instance.
+    assert.equal(muxPlayerEl.media.streamType, 'live');
+  });
+
+  it('should apply noautoseektolive to theme for DVR stream types', async () => {
+    const muxPlayerEl = await fixture(`<mux-player
+    ></mux-player>`);
+    assert(!muxPlayerEl.mediaTheme.hasAttribute('noautoseektolive'));
+    muxPlayerEl.streamType = 'live:dvr';
+    assert(muxPlayerEl.mediaTheme.hasAttribute('noautoseektolive'));
+    muxPlayerEl.streamType = undefined;
+    assert(!muxPlayerEl.mediaTheme.hasAttribute('noautoseektolive'));
+    muxPlayerEl.streamType = 'll-live:dvr';
+    assert(muxPlayerEl.mediaTheme.hasAttribute('noautoseektolive'));
+  });
+});
+
 // skip cuepoint tests on all browsers
 // TODO fixup cuepoint tests and behavior across browsers
 describe.skip('Feature: cuePoints', async () => {
