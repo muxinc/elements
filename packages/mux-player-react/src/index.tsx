11e2a3d7ebd02f63e2ce071500442a61c188f800
Christian Pillsbury <cpillsbury@mux.com>
2023-04-13 13:54:12 -0700

    fix(mux-player-react): Have smarter value checking for currentTime to avoid unintentional 'resets' of currentTime to 0.

--- packages/mux-player-react/src/index.tsx
+++ packages/mux-player-react/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { CSSProperties } from 'react';
 import type { StreamTypes, PlaybackTypes, CmcdTypes } from '@mux/playback-core';
 import { MediaError } from '@mux/mux-player';
@@ -194,9 +194,26 @@ const usePlayer = (
       return defaultHasChanged(playerEl, value, propName);
     }
   );
-  // NOTE: Somewhere in the codebase, `currentTime` is getting cast to a number, resulting in `NaN` + an error.
-  // This is a bandaid solution for now. (CJP)
-  useObjectPropEffect('currentTime', currentTime ?? 0, ref);
+  // NOTE: Need to handle currentTime differently to manage
+  // 1. Whether the prop value is/was "unset" to e.g. `undefined`
+  //    - To make sure we don't try to update currentTime on these changes
+  // 2. Whether the *prop* value was changed ("from the outside")
+  //    - To only apply the `currentTime` if it actually changed from the last provided value
+  // (CJP)
+  const [internalCurrentTime, setInternalCurrentTime] = useState(currentTime);
+  useEffect(() => {
+    if (internalCurrentTime == currentTime) return;
+    setInternalCurrentTime(currentTime);
+  }, [currentTime, internalCurrentTime]);
+  useObjectPropEffect(
+    'currentTime',
+    internalCurrentTime,
+    ref,
+    (playerEl: HTMLMediaElement, currentTimeVal?: number) => {
+      if (currentTimeVal == null) return;
+      playerEl.currentTime = currentTimeVal;
+    }
+  );
   useEventCallbackEffect('abort', ref, onAbort);
   useEventCallbackEffect('canplay', ref, onCanPlay);
   useEventCallbackEffect('canplaythrough', ref, onCanPlayThrough);
