3e900330cf7cbfef0e96cb3d112e74adfe39e3be
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-04-17 12:21:12 -0400

    chore(mux-uploader-react): Export new modules for React use (#659)
    This PR creates wrappers for each web component for use within a React
    application. Also removes some outdated styles which are no longer in
    `mux-uploader`
    
    ---------
    
    Co-authored-by: Christian Pillsbury <cpillsbury@mux.com>

---
+++ packages/mux-uploader-react/src/mux-uploader-file-select.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '@mux/mux-uploader';
+import type MuxUploaderFileSelectElement from '@mux/mux-uploader';
+import { toNativeProps } from './common/utils';
+import { useRef } from 'react';
+import { useCombinedRefs } from './useCombinedRefs';
+
+export type MuxUploaderFileSelectRefAttributes = MuxUploaderFileSelectElement;
+
+export type MuxUploaderFileSelectProps = {
+  muxUploader?: string;
+  children?: React.ReactNode;
+};
+
+const MuxUploaderFileSelectInternal = React.forwardRef<MuxUploaderFileSelectRefAttributes, MuxUploaderFileSelectProps>(
+  ({ children, ...props }, ref) => {
+    return React.createElement('mux-uploader-file-select', toNativeProps({ ...props, ref }), children);
+  }
+);
+
+const MuxUploaderFileSelect = React.forwardRef<MuxUploaderFileSelectRefAttributes, MuxUploaderFileSelectProps>(
+  (props, ref) => {
+    const innerUploaderFileSelectRef = useRef<MuxUploaderFileSelectElement>(null);
+    const uploaderFileSelectRef = useCombinedRefs(innerUploaderFileSelectRef, ref);
+
+    return (
+      <MuxUploaderFileSelectInternal ref={uploaderFileSelectRef as typeof innerUploaderFileSelectRef} {...props} />
+    );
+  }
+);
+
+export default MuxUploaderFileSelect;
