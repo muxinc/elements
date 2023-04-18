529be278b97fb824bf2a973718a1dbb0d26624af
Adam Jaggard <ajaggard@mux.com>
2023-04-18 11:24:07 +0100

    use globalThis to define theme custom element

--- packages/mux-player/src/themes/classic/index.ts
+++ packages/mux-player/src/themes/classic/index.ts
@@ -2,7 +2,6 @@
 import theme from './classic.html';
 import { document } from '../../polyfills';
 import { MediaThemeElement } from 'media-chrome/dist/media-theme-element';
-import { window } from 'media-chrome/dist/utils/server-safe-globals.js';
 
 const template = document.createElement('template');
 if ('innerHTML' in template) template.innerHTML = theme;
@@ -11,8 +10,8 @@ class MediaThemeClassic extends MediaThemeElement {
   static template = template.content.children[0];
 }
 
-if (!window.customElements.get('media-theme-classic')) {
-  window.customElements.define('media-theme-classic', MediaThemeClassic);
+if (!globalThis.customElements.get('media-theme-classic')) {
+  globalThis.customElements.define('media-theme-classic', MediaThemeClassic);
 }
 
 export default MediaThemeClassic;
