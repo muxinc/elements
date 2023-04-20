be75e6b19c44ebca1a9791b59e43c703f7d0acd9
Gary Katsevman <gary@mux.com>
2023-04-20 17:12:49 -0400

    chore: numerically sort version numbers for canary release

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -126,7 +126,7 @@ function canary {
 
   # get last published version from NPM without alpha / beta, remove -SHA hash
 
-  # debug jq command at https://jqplay.org/s/Wwjv5hVUCL0
+  # debug jq command at https://jqplay.org/s/Vnz8ioZ2vtm
   #
   #   1. remove alpha or beta versions
   #   2 & 3. convert to { version: 'x.x.x', dist: 'canary', build: x }
@@ -138,7 +138,10 @@ function canary {
     jq -r '. - map(select(contains("alpha") or contains("beta")))
       | map(capture("(?<version>\\d+\\.\\d+\\.\\d+)(-(?<dist>[a-z]+))?(\\.(?<build>\\d+))?"))
       | map(.build? |= (. // 0 | tonumber))
-      | sort_by(.version, .build)
+      | map(.patch? |= (. // 0 | tonumber))
+      | map(.minor? |= (. // 0 | tonumber))
+      | map(.major? |= (. // 0 | tonumber))
+      | sort_by(.major, .minor, .patch, .build)
       | map(.version + "-" + (.dist // "latest") + "." + (.build|tostring))
       | last')
 
