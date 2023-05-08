c0804506567a6b45432dc391ce93a7fce6f70d12
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-05-08 11:34:31 -0400

    Updating parts names

--- packages/mux-uploader/src/layouts/block.ts
+++ packages/mux-uploader/src/layouts/block.ts
@@ -14,25 +14,19 @@ export default function blockLayout(contextElement: MuxUploaderElement): Documen
   const progressElements = conditionalRender(
     noProgress,
     `
-      <mux-uploader-progress part="mux-uploader-progress" type="percentage"></mux-uploader-progress>
-      <mux-uploader-progress part="mux-uploader-progress"></mux-uploader-progress>
+      <mux-uploader-progress part="progress" type="percentage"></mux-uploader-progress>
+      <mux-uploader-progress part="progress"></mux-uploader-progress>
     `
   );
-  const statusElement = conditionalRender(
-    noStatus,
-    '<mux-uploader-status part="mux-uploader-status"></mux-uploader-status>'
-  );
-  const retryElement = conditionalRender(
-    noRetry,
-    '<mux-uploader-retry part="mux-uploader-retry"></mux-uploader-retry>'
-  );
+  const statusElement = conditionalRender(noStatus, '<mux-uploader-status part="status"></mux-uploader-status>');
+  const retryElement = conditionalRender(noRetry, '<mux-uploader-retry part="retry"></mux-uploader-retry>');
 
   return document.createRange().createContextualFragment(`
     <${wrapper}>
       ${statusElement}
       ${retryElement}
 
-      <mux-uploader-file-select part="mux-uploader-file-select">
+      <mux-uploader-file-select part="file-select" exportparts="file-select-button">
         <slot name="file-select">
           ${fileSelectFragment}
         </slot>
