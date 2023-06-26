ad79445e2fde2d4aeb9c6439d25760fd3b83db89
Wesley Luyten <me@wesleyluyten.com>
2023-06-26 15:59:38 -0500

    fix: simplify, remove explicit server action

--- packages/mux-uploader-react/src/rsc.tsx
+++ packages/mux-uploader-react/src/rsc.tsx
@@ -5,20 +5,12 @@ import MuxUploaderClient from './index';
 const { Video } = new Mux();
 
 export default async function MuxUploaderServer(props: any) {
-  async function getUploadUrl() {
-    'use server';
+  const upload = await Video.Uploads.create({
+    cors_origin: '*',
+    new_asset_settings: {
+      playback_policy: 'public',
+    },
+  });
 
-    const upload = await Video.Uploads.create({
-      cors_origin: '*',
-      new_asset_settings: {
-        playback_policy: 'public',
-      },
-    });
-
-    return upload.url;
-  }
-
-  const url = await getUploadUrl();
-
-  return <MuxUploaderClient endpoint={url} {...props} />;
+  return <MuxUploaderClient endpoint={upload.url} {...props} />;
 }
