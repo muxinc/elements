cc551289dd9af6beb3234e71ad9c94b43c11839e
Christian Pillsbury <cpillsbury@mux.com>
2023-07-06 18:39:08 -0700

    fix(playback-core): Switch back to using the unminified hls.js module, since this appears to cause edge case issues with initial times after ts segment transmuxing.

--- packages/playback-core/src/hls.ts
+++ packages/playback-core/src/hls.ts
@@ -1,9 +1,10 @@
-// @ts-ignore
-import HlsMin from 'hls.js/dist/hls.min.js';
+import Hls from 'hls.js';
+// // @ts-ignore
+// import HlsMin from 'hls.js/dist/hls.min.js';
 import type HlsClassType from 'hls.js';
 
-type Hls = typeof HlsClassType & HlsClassType;
-const Hls: Hls = HlsMin as unknown as Hls;
+// type Hls = typeof HlsClassType & HlsClassType;
+// const Hls: Hls = HlsMin as unknown as Hls;
 
 export default Hls;
 export type HlsInterface = HlsClassType;
