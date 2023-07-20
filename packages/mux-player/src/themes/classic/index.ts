327809995be207a53f2d1d82db6d34a6936ce20d
Adam Jaggard <ajaggard@mux.com>
2023-07-20 14:51:31 +0100

    fix media theme element import

--- packages/mux-player/src/themes/classic/index.ts
+++ packages/mux-player/src/themes/classic/index.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import theme from './classic.html';
 import { document, globalThis } from '../../polyfills';
-import { MediaThemeElement } from 'media-chrome/dist/media-theme-element';
+import { MediaThemeElement } from 'media-chrome/dist/media-theme-element.js';
 
 const template = document.createElement('template');
 if ('innerHTML' in template) template.innerHTML = theme;
