d0ea87a86f32b04d91c50af34fff31011dedf11a
Adam Jaggard <ajaggard@mux.com>
2023-07-25 15:57:30 +0100

    fix default theme template access

--- packages/mux-player/src/themes/default/index.ts
+++ packages/mux-player/src/themes/default/index.ts
@@ -7,7 +7,7 @@ const template = document.createElement('template');
 if ('innerHTML' in template) template.innerHTML = theme;
 
 class MediaThemeDefault extends MediaThemeElement {
-  static template = template.content.children[0];
+  static template = template.content?.children?.[0];
 }
 
 if (!globalThis.customElements.get('media-theme-default')) {
