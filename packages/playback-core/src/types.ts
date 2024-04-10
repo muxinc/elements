2162973f4fa54af882c1fc7be4bab88de6b18ccc
Wesley Luyten <me@wesleyluyten.com>
2024-04-10 12:21:51 -0500

    fix: add mux-embed types reference (#899)

--- packages/playback-core/src/types.ts
+++ packages/playback-core/src/types.ts
@@ -1,3 +1,5 @@
+/* eslint @typescript-eslint/triple-slash-reference: "off" */
+/// <reference path="../../../node_modules/mux-embed/dist/types/mux-embed.d.ts" />
 import type { Options } from 'mux-embed';
 import type { MediaError } from './errors';
 import type { HlsConfig } from 'hls.js';
