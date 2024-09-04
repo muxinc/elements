2d3197c27ed476246bf5f54013e3237c487ccb1b
Christian Pillsbury <cpillsbury@mux.com>
2024-09-04 09:35:35 -0700

    build(playback-core): attempt to refactor esbuilder et al. to work for playback-core lang crud.

--- packages/playback-core/src/util.ts
+++ packages/playback-core/src/util.ts
@@ -1,5 +1,7 @@
 import { isKeyOf, ExtensionMimeTypeMap, MimeTypeShorthandMap, StreamTypes } from './types';
 import type { HlsPlaylistTypes, MuxMediaProps } from './types';
+// @ts-ignore
+import lang from '../lang/en.json';
 
 type addEventListenerWithTeardown = <
   K extends keyof HTMLMediaElementEventMap,
@@ -145,65 +147,6 @@ export const isJWTAudMismatch = ({ aud }: Partial<Pick<MuxJWT, 'aud'>>, expected
   return aud !== expectedAud;
 };
 
-export const lang: { [k: string]: string } = {
-  '(opens in a new window)': '(opens in a new window)',
-  'Network Error': 'Network Error',
-  '{category} is not currently available': '{category} is not currently available',
-  'Attempting to play DRM-protected content without providing a DRM token.':
-    'Attempting to play DRM-protected content without providing a DRM token.',
-  'Cannot play DRM-protected content with current security configuration on this browser. Try playing in another browser.':
-    'Cannot play DRM-protected content with current security configuration on this browser. Try playing in another browser.',
-  'Failed to generate a DRM license request. This may be an issue with the player or your protected content.':
-    'Failed to generate a DRM license request. This may be an issue with the player or your protected content.',
-  'Failed to update DRM license. This may be an issue with the player or your protected content.':
-    'Failed to update DRM license. This may be an issue with the player or your protected content.',
-  'Your server certificate failed when attempting to set it. This may be an issue with a no longer valid certificate.':
-    'Your server certificate failed when attempting to set it. This may be an issue with a no longer valid certificate.',
-  'The DRM Content Decryption Module system had an internal failure. Try reloading the page, upading your browser, or playing in another browser.':
-    'The DRM Content Decryption Module system had an internal failure. Try reloading the page, upading your browser, or playing in another browser.',
-  'DRM playback is being attempted in an environment that is not sufficiently secure. User may see black screen.':
-    'DRM playback is being attempted in an environment that is not sufficiently secure. User may see black screen.',
-  'The live stream or video file are not yet ready.': 'The live stream or video file are not yet ready.',
-  'This playback-id may belong to a live stream that is not currently active or an asset that is not ready.':
-    'This playback-id may belong to a live stream that is not currently active or an asset that is not ready.',
-  '{category} does not exist': '{category} does not exist',
-  'This URL or playback-id does not exist. You may have used an Asset ID or an ID from a different resource.':
-    'This URL or playback-id does not exist. You may have used an Asset ID or an ID from a different resource.',
-  'The specificed playback ID {playbackId} contains a token which must be provided via the playback-token attribute.':
-    'The specificed playback ID {playbackId} contains a token which must be provided via the playback-token attribute.',
-  'Invalid {categoryName} URL': 'Invalid {categoryName} URL',
-  'The video URL or {tokenNamePrefix}-token are formatted with incorrect or incomplete information.':
-    'The video URL or {tokenNamePrefix}-token are formatted with incorrect or incomplete information.',
-  '{category} URL has expired': '{category} URL has expired',
-  'The video’s secured {tokenNamePrefix}-token has expired.':
-    'The video’s secured {tokenNamePrefix}-token has expired.',
-  'Expired at: {expiredDate}. Current time: {currentDate}.': 'Expired at: {expiredDate}. Current time: {currentDate}.',
-  '{category} URL is formatted incorrectly': '{category} URL is formatted incorrectly',
-  'The video’s playback ID does not match the one encoded in the {tokenNamePrefix}-token.':
-    'The video’s playback ID does not match the one encoded in the {tokenNamePrefix}-token.',
-  'Specified playback ID: {playbackId} and the playback ID encoded in the {tokenNamePrefix}-token: {tokenPlaybackId}':
-    'Specified playback ID: {playbackId} and the playback ID encoded in the {tokenNamePrefix}-token: {tokenPlaybackId}',
-  'The {tokenNamePrefix}-token is formatted with incorrect information.':
-    'The {tokenNamePrefix}-token is formatted with incorrect information.',
-  'The {tokenNamePrefix}-token has an incorrect aud value: {aud}. aud value should be {expectedAud}.':
-    'The {tokenNamePrefix}-token has an incorrect aud value: {aud}. aud value should be {expectedAud}.',
-  'The {tokenNamePrefix}-token has no aud value. aud value should be {expectedAud}.':
-    'The {tokenNamePrefix}-token has no aud value. aud value should be {expectedAud}.',
-  'Authorization error trying to access this {category} URL. If this is a signed URL, you might need to provide a {tokenNamePrefix}-token.':
-    'Authorization error trying to access this {category} URL. If this is a signed URL, you might need to provide a {tokenNamePrefix}-token.',
-  'Media Error': 'Media Error',
-  'Source Not Supported': 'Source Not Supported',
-  Error: 'Error',
-  'Your device appears to be offline': 'Your device appears to be offline',
-  'Check your internet connection and try reloading this video.':
-    'Check your internet connection and try reloading this video.',
-  'Invalid stream-type value supplied: `{streamType}`. Please provide stream-type as either: `on-demand` or `live`':
-    'Invalid stream-type value supplied: `{streamType}`. Please provide stream-type as either: `on-demand` or `live`',
-  'The URL or playback-id was invalid. You may have used an invalid value as a playback-id.':
-    'The URL or playback-id was invalid. You may have used an invalid value as a playback-id.',
-  'Read more: ': 'Read more: ',
-};
-
 const DEFAULT_LOCALE = 'en';
 
 // NL example
@@ -211,8 +154,8 @@ const DEFAULT_LOCALE = 'en';
 //   "Network Error": "Netwerk Fout",
 // };
 export function i18n(str: string, translate = true): any {
-  const message = translate ? lang[str] ?? str : str;
-  const locale = translate ? lang.code : DEFAULT_LOCALE;
+  const message = translate ? (lang as unknown as any)?.[str] ?? str : str;
+  const locale = translate ? (lang as unknown as any).code : DEFAULT_LOCALE;
   return new IntlMessageFormat(message, locale);
 }
 
@@ -224,7 +167,8 @@ class IntlMessageFormat {
   message: string;
   locale: string;
 
-  constructor(message: string, locale = lang.code ?? DEFAULT_LOCALE) {
+  /** @TODO re-implement esbuild custom plugin for code usage (CJP) */
+  constructor(message: string, locale = (lang as unknown as any) ?? DEFAULT_LOCALE) {
     this.message = message;
     this.locale = locale;
   }
