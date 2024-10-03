10884436604bfe323552d85319c848d037859178
Christian Pillsbury <cjpillsbury@gmail.com>
2024-10-03 13:38:12 -0700

    fix(playback-core): Handle cases where subs or cc media playlist is default (based on hls.js assumptions). (#997)
    fixes https://github.com/muxinc/player-and-sdks-team/issues/168

--- packages/playback-core/src/text-tracks.ts
+++ packages/playback-core/src/text-tracks.ts
@@ -25,13 +25,12 @@ export function setupTextTracks(
         return lang == baseTrackObj?.lang && name === trackObj.label && type.toLowerCase() === trackObj.kind;
       });
 
-      addTextTrack(
-        mediaEl,
-        trackObj.kind as TextTrackKind,
-        trackObj.label,
-        baseTrackObj?.lang,
-        `${trackObj.kind}${idx}`
-      );
+      // NOTE: Undocumented method for determining identifier by hls.js. Relied on for
+      // ensuring CUES_PARSED events can identify and apply cues to the appropriate track (CJP).
+      // See: https://github.com/video-dev/hls.js/blob/master/src/controller/timeline-controller.ts#L640
+      const id = trackObj._id ?? trackObj.default ? 'default' : `${trackObj.kind}${idx}`;
+
+      addTextTrack(mediaEl, trackObj.kind as TextTrackKind, trackObj.label, baseTrackObj?.lang, id, trackObj.default);
     });
   });
 
@@ -42,17 +41,29 @@ export function setupTextTracks(
       return textTrack.id && textTrack.mode === 'showing' && ['subtitles', 'captions'].includes(textTrack.kind);
     });
 
+    if (!showingTrack) return;
+
+    const currentHlsTrack = hls.subtitleTracks[hls.subtitleTrack];
+
     // If hls.subtitleTrack is -1 or its id changed compared to the one that is showing load the new subtitle track.
-    const hlsTrackId = `${hls.subtitleTracks[hls.subtitleTrack]?.type.toLowerCase()}${hls.subtitleTrack}`;
-    if (showingTrack && (hls.subtitleTrack < 0 || showingTrack?.id !== hlsTrackId)) {
-      const idx = hls.subtitleTracks.findIndex(({ lang, name, type }) => {
-        return lang == showingTrack.language && name === showingTrack.label && type.toLowerCase() === showingTrack.kind;
+    const hlsTrackId = !currentHlsTrack
+      ? undefined
+      : currentHlsTrack.default
+        ? 'default'
+        : `${hls.subtitleTracks[hls.subtitleTrack].type.toLowerCase()}${hls.subtitleTrack}`;
+
+    if (hls.subtitleTrack < 0 || showingTrack?.id !== hlsTrackId) {
+      const idx = hls.subtitleTracks.findIndex(({ lang, name, type, default: defaultTrack }) => {
+        return (
+          (showingTrack.id === 'default' && defaultTrack) ||
+          (lang == showingTrack.language && name === showingTrack.label && type.toLowerCase() === showingTrack.kind)
+        );
       });
       // After the subtitleTrack is set here, hls.js will load the playlist and CUES_PARSED events will be fired below.
       hls.subtitleTrack = idx;
     }
 
-    if (showingTrack && showingTrack?.id === hlsTrackId) {
+    if (showingTrack?.id === hlsTrackId) {
       // Refresh the cues after a texttrack mode change to fix a Chrome bug causing the captions not to render.
       if (showingTrack.cues) {
         Array.from(showingTrack.cues).forEach((cue) => {
@@ -125,7 +136,8 @@ export function addTextTrack(
   kind: TextTrackKind,
   label: string,
   lang?: string,
-  id?: string
+  id?: string,
+  defaultTrack?: boolean
 ): TextTrack {
   const trackEl = document.createElement('track');
   trackEl.kind = kind;
@@ -136,6 +148,9 @@ export function addTextTrack(
   }
   if (id) {
     trackEl.id = id;
+  }
+  if (!!defaultTrack) {
+    trackEl.default = true;
   }
   trackEl.track.mode = ['subtitles', 'captions'].includes(kind) ? 'disabled' : 'hidden';
 
