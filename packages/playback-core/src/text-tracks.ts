b24b967c17798bc98a64e7a3e1a8725ef0c1a0b9
Christian Pillsbury <cpillsbury@mux.com>
2024-10-03 12:03:49 -0700

    fix(playback-core): Account for same naming woes when switching or toggling subs or CCs.

--- packages/playback-core/src/text-tracks.ts
+++ packages/playback-core/src/text-tracks.ts
@@ -41,17 +41,25 @@ export function setupTextTracks(
       return textTrack.id && textTrack.mode === 'showing' && ['subtitles', 'captions'].includes(textTrack.kind);
     });
 
+    if (!showingTrack) return;
+
     // If hls.subtitleTrack is -1 or its id changed compared to the one that is showing load the new subtitle track.
-    const hlsTrackId = `${hls.subtitleTracks[hls.subtitleTrack]?.type.toLowerCase()}${hls.subtitleTrack}`;
-    if (showingTrack && (hls.subtitleTrack < 0 || showingTrack?.id !== hlsTrackId)) {
-      const idx = hls.subtitleTracks.findIndex(({ lang, name, type }) => {
-        return lang == showingTrack.language && name === showingTrack.label && type.toLowerCase() === showingTrack.kind;
+    const hlsTrackId =
+      showingTrack.id === 'default'
+        ? 'default'
+        : `${hls.subtitleTracks[hls.subtitleTrack]?.type.toLowerCase()}${hls.subtitleTrack}`;
+    if (hls.subtitleTrack < 0 || showingTrack?.id !== hlsTrackId) {
+      const idx = hls.subtitleTracks.findIndex(({ lang, name, type, default: defaultTrack }) => {
+        return (
+          (showingTrack.id === 'default' && defaultTrack) ||
+          (lang == showingTrack.language && name === showingTrack.label && type.toLowerCase() === showingTrack.kind)
+        );
       });
       // After the subtitleTrack is set here, hls.js will load the playlist and CUES_PARSED events will be fired below.
       hls.subtitleTrack = idx;
     }
 
-    if (showingTrack && showingTrack?.id === hlsTrackId) {
+    if (showingTrack?.id === hlsTrackId) {
       // Refresh the cues after a texttrack mode change to fix a Chrome bug causing the captions not to render.
       if (showingTrack.cues) {
         Array.from(showingTrack.cues).forEach((cue) => {
