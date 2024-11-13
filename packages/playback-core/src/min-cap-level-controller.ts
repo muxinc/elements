576623b1ca63590ecf66dac69d7386122ed27266
Christian Pillsbury <cpillsbury@mux.com>
2024-11-13 08:03:42 -0800

    fix(playback-core): Adjusting min max cap logic to use explicit resolution cap value.

--- packages/playback-core/src/min-cap-level-controller.ts
+++ packages/playback-core/src/min-cap-level-controller.ts
@@ -1,40 +1,59 @@
-import Hls, { CapLevelController, Level } from 'hls.js';
+import type Hls from 'hls.js';
+import type { ILogger, Level } from 'hls.js';
+import { CapLevelController } from 'hls.js';
 
 /**
  * A custom HLS.js CapLevelController that behaves like the default one, except
  * it enforces a "minimum maximum" to avoid forced capping to lower quality at small sizes
  */
 class MinCapLevelController extends CapLevelController {
+  // Never cap below this level.
+  static minMaxResolution = 720;
+
   constructor(hls: Hls) {
     super(hls);
   }
 
-  getMaxLevel(capLevelIndex: number) {
-    const baseMaxLevel = super.getMaxLevel(capLevelIndex);
+  get levels() {
     // NOTE: hls is a TS-private member in CapLevelController. Should be TS-protected (CJP)
     // @ts-ignore
-    const levels = this.hls.levels as Level[];
-    if (!levels.length) {
-      return -1;
-    }
+    return (this.hls.levels ?? []) as Level[];
+  }
 
-    const validLevels = levels.filter(
+  getValidLevels(capLevelIndex: number) {
+    return this.levels.filter(
       // NOTE: isLevelAllowed is a TS-private member in CapLevelController. Should be TS-protected (CJP)
       // @ts-ignore
       (level, index) => this.isLevelAllowed(level) && index <= capLevelIndex
     );
+  }
 
-    if (
-      validLevels[baseMaxLevel] &&
-      // If the default CapLevelController's maxLevel selection is <= 480p (portrait or landscape),
-      // let the playback engine still have the option of the next higher resolution
-      Math.min(validLevels[baseMaxLevel].width, validLevels[baseMaxLevel].height) <= 480 &&
-      baseMaxLevel < validLevels.length - 2
-    ) {
-      return baseMaxLevel + 1;
-    }
+  getMaxLevel(capLevelIndex: number) {
+    const baseMaxLevel = super.getMaxLevel(capLevelIndex);
+    const validLevels = this.getValidLevels(capLevelIndex);
 
-    return baseMaxLevel;
+    // Default maxLevel selection ended up out of bounds to indicate (e.g. -1), so use it
+    if (!validLevels[baseMaxLevel]) return baseMaxLevel;
+
+    const baseMaxLevelResolution = Math.min(validLevels[baseMaxLevel].width, validLevels[baseMaxLevel].height);
+    // Use this syntax to grab static on the off chance of subclassing/best practice
+    const preferredMinMaxResolution = (this.constructor as typeof MinCapLevelController).minMaxResolution;
+
+    // Default maxLevel selection already meets our conditions, so use it
+    if (baseMaxLevelResolution >= preferredMinMaxResolution) return baseMaxLevel;
+
+    // Default maxLevel selection is below the preferred "min max", so find the lowest level
+    // that is >= the preference. We can simply repurpose CapLevelController:getMaxLevelByMediaSize()
+    // for this, "lying" about the element's size.
+    // NOTE: Since CapLevelController:getMaxLevelByMediaSize() uses "max square size" under the hood
+    // already, we don't need to duplicate that logic here.
+    const maxLevel = CapLevelController.getMaxLevelByMediaSize(
+      validLevels,
+      preferredMinMaxResolution * (16 / 9),
+      preferredMinMaxResolution
+    );
+
+    return maxLevel;
   }
 }
 
