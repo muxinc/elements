6350a9bf143a4cc0edcde5acabd3ae88f14609e7
Wesley Luyten <me@wesleyluyten.com>
2025-04-09 17:01:06 -0500

    fix: respect hls.js fatal error flag if available (#1099)
    fixes an issue where playback is not broken yet but the player throws a
    fatal error making the error dialog show up while the video is playing
    underneath.

--- packages/playback-core/src/index.ts
+++ packages/playback-core/src/index.ts
@@ -1415,7 +1415,8 @@ const getErrorFromHlsErrorData = (
   const errorCode = hlsErrorDataToErrorCode(data);
   if (errorCode === MediaError.MEDIA_ERR_NETWORK && data.response) {
     const category = hlsErrorDataToCategory(data) ?? MuxErrorCategory.VIDEO;
-    mediaError = getErrorFromResponse(data.response, category, props) ?? new MediaError('', errorCode);
+    mediaError =
+      getErrorFromResponse(data.response, category, props, data.fatal) ?? new MediaError('', errorCode, data.fatal);
   } else if (errorCode === MediaError.MEDIA_ERR_ENCRYPTED) {
     if (data.details === Hls.ErrorDetails.KEY_SYSTEM_NO_CONFIGURED_LICENSE) {
       const message = i18n('Attempting to play DRM-protected content without providing a DRM token.');
