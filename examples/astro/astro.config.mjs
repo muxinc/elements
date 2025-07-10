aea8a566261acf0c05a6eae7f107ef6154becdc4
Matt Kane <m@mk.gg>
2025-07-10 11:17:06 +0100

    Clean up example site config

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -2,11 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  // adapter: vercel(),
+  adapter: vercel(),
   devToolbar: {
     enabled: false,
   },
