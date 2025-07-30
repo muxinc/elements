bee5d558803d0e1703d1acee92cf98bf3132bdd4
Christian Pillsbury <cpillsbury@mux.com>
2025-07-30 11:30:43 -0700

    chore: Chapters examples to demonstrate functionality with native playback.

--- examples/nextjs-with-typescript/pages/MuxPlayerChapters.tsx
+++ examples/nextjs-with-typescript/pages/MuxPlayerChapters.tsx
@@ -1,33 +1,40 @@
 import Link from "next/link";
 import Head from 'next/head';
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import MuxPlayer from "@mux/mux-player-react";
 import type MuxPlayerElement from "@mux/mux-player";
 
 type Chapter = { startTime: number; endTime: number; value: string; };
-const chapters: Chapter[] = [
-  { startTime: 0, endTime: 3, value: 'Chapter 1' },
-  { startTime: 3, endTime: 6, value: 'Chapter 2' },
-  { startTime: 6, endTime: 9, value: 'Chapter 3' }
+const exampleChapters: Chapter[] = [
+  { startTime: 0, endTime: 3, value: 'A beginning chapter' },
+  { startTime: 3, endTime: 6, value: 'In the middle' },
+  { startTime: 6, endTime: 9, value: 'Is this the end? (No)' }
 ];
 
 
-function MuxPlayerPage() {
+function MuxPlayerPage({ chapters = exampleChapters }: { chapters: Chapter[] }) {
   const [activeChapter, setActiveChapter] = useState<Chapter>(undefined);
 
-  function addChaptersToPlayer(playerEl: MuxPlayerElement) {
+  const addChaptersToPlayer = useCallback((playerEl: MuxPlayerElement) => {
+    // Not ready for chapters yet!
+    if (!playerEl || !playerEl.readyState) return;
+    // Already added chapters!
+    if (!!playerEl.chapters?.length) return;
     playerEl.addChapters(chapters);
-  }
+  }, [chapters]);
 
   return (
     <>
       <Head>
-        <title>&lt;MuxPlayer/&gt; (CuePoints) Demo</title>
+        <title>&lt;MuxPlayer/&gt; (Chapters) Demo</title>
       </Head>
 
       <MuxPlayer
+        key="23s11nz72DsoN657h4314PjKKjsF2JG33eBQQt6B95I"
+        ref={addChaptersToPlayer}
         playbackId="23s11nz72DsoN657h4314PjKKjsF2JG33eBQQt6B95I"
-        preload="auto"
+        // Using native playback
+        preferPlayback="mse"
         onLoadedMetadata={({ target }) => {
           addChaptersToPlayer(target as MuxPlayerElement);
         }}
